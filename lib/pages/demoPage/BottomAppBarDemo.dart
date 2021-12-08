7d2304fac4dcc971a697dd9405d105a58eb098d1
shangbinbin <shangbinbin@haier.com>
2021-12-08 15:23:57 +0800

    add wiget demo

---
+++ lib/pages/demoPage/BottomAppBarDemo.dart
@@ -0,0 +1,154 @@
+//
+//  BottomAppBarDemo.dart
+//  flutter_templet_project
+//
+//  Created by shang on 12/7/21 5:47 PM.
+//  Copyright © 12/7/21 shang. All rights reserved.
+//
+
+import 'package:flutter/material.dart';
+
+class BottomAppBarDemo extends StatefulWidget {
+  const BottomAppBarDemo({Key? key}) : super(key: key);
+
+  @override
+  State createState() => _BottomAppBarDemoState();
+}
+
+class _BottomAppBarDemoState extends State<BottomAppBarDemo> {
+  bool _showFab = true;
+  bool _showNotch = true;
+  FloatingActionButtonLocation _fabLocation =
+      FloatingActionButtonLocation.endDocked;
+
+  void _onShowNotchChanged(bool value) {
+    setState(() {
+      _showNotch = value;
+    });
+  }
+
+  void _onShowFabChanged(bool value) {
+    setState(() {
+      _showFab = value;
+    });
+  }
+
+  void _onFabLocationChanged(FloatingActionButtonLocation? value) {
+    setState(() {
+      _fabLocation = value ?? FloatingActionButtonLocation.endDocked;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // automaticallyImplyLeading: false,
+          title: const Text('Bottom App Bar Demo'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.only(bottom: 88),
+          children: <Widget>[
+            SwitchListTile(
+              title: const Text(
+                'Floating Action Button',
+              ),
+              value: _showFab,
+              onChanged: _onShowFabChanged,
+            ),
+            SwitchListTile(
+              title: const Text('Notch'),
+              value: _showNotch,
+              onChanged: _onShowNotchChanged,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text('Floating action button position'),
+            ),
+            RadioListTile<FloatingActionButtonLocation>(
+              title: const Text('Docked - End'),
+              value: FloatingActionButtonLocation.endDocked,
+              groupValue: _fabLocation,
+              onChanged: _onFabLocationChanged,
+            ),
+            RadioListTile<FloatingActionButtonLocation>(
+              title: const Text('Docked - Center'),
+              value: FloatingActionButtonLocation.centerDocked,
+              groupValue: _fabLocation,
+              onChanged: _onFabLocationChanged,
+            ),
+            RadioListTile<FloatingActionButtonLocation>(
+              title: const Text('Floating - End'),
+              value: FloatingActionButtonLocation.endFloat,
+              groupValue: _fabLocation,
+              onChanged: _onFabLocationChanged,
+            ),
+            RadioListTile<FloatingActionButtonLocation>(
+              title: const Text('Floating - Center'),
+              value: FloatingActionButtonLocation.centerFloat,
+              groupValue: _fabLocation,
+              onChanged: _onFabLocationChanged,
+            ),
+          ],
+        ),
+        floatingActionButton: _showFab
+            ? FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+                tooltip: 'Create',
+              )
+            : null,
+        floatingActionButtonLocation: _fabLocation,
+        bottomNavigationBar: _DemoBottomAppBar(
+          fabLocation: _fabLocation,
+          shape: _showNotch ? const CircularNotchedRectangle() : null,
+        ),
+      );
+  }
+}
+
+class _DemoBottomAppBar extends StatelessWidget {
+  const _DemoBottomAppBar({
+    this.fabLocation = FloatingActionButtonLocation.endDocked,
+    this.shape = const CircularNotchedRectangle(),
+  });
+
+  final FloatingActionButtonLocation fabLocation;
+  final NotchedShape? shape;
+
+  static final List<FloatingActionButtonLocation> centerLocations = [
+    FloatingActionButtonLocation.centerDocked,
+    FloatingActionButtonLocation.centerFloat,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: shape,
+      color: Colors.blue,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              tooltip: 'Open navigation menu',
+              icon: const Icon(Icons.menu),
+              onPressed: () {},
+            ),
+            if (centerLocations.contains(fabLocation)) const Spacer(),
+            IconButton(
+              tooltip: 'Search',
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Favorite',
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/demoPage/ChipDemo.dart
@@ -0,0 +1,260 @@
+//
+//  ChipDemo.dart
+//  flutter_templet_project
+//
+//  Created by shang on 12/7/21 6:13 PM.
+//  Copyright © 12/7/21 shang. All rights reserved.
+//
+
+import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/extensions/ddlog.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+
+class ChipDemo extends StatefulWidget {
+  final String? title;
+
+  ChipDemo({Key? key, this.title}) : super(key: key);
+
+  @override
+  _ChipDemoState createState() => _ChipDemoState();
+}
+
+class _ChipDemoState extends State<ChipDemo> {
+  @override
+  Widget build(BuildContext context) {
+    dynamic arguments = ModalRoute.of(context)!.settings.arguments;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title ?? "$widget"),
+        ),
+        body: buildWrap(),
+    );
+  }
+
+  int? _value = 1;
+
+  Wrap buildWrap() {
+    List<int> titles = List.generate(3, (index) => index);
+    return Wrap(
+      spacing: 8.0, // 主轴(水平)方向间距
+      runSpacing: -8.0, // 纵轴（垂直）方向间距
+      alignment: WrapAlignment.start, //沿主轴方向居中
+      children: [
+        Chip(
+          label: Text("Chip",),
+        ),
+        Chip(
+          label: Text("Chip",),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(2.0),
+            )
+        ),
+        Divider(),
+        Chip(
+          label: Text("带 deleteIcon的 Chip",),
+          deleteIcon: Icon( Icons.close, ),
+          onDeleted: () {setState(() {ddlog("onDeleted: 带 deleteIcon的 Chip");}); },
+          deleteButtonTooltipMessage: "弹出提示",
+        ),
+        Divider(),
+        Chip(
+          label: Text("带 avatar 和 deleteIcon的 Chip",),
+          avatar: Image.asset("images/avatar.png", fit: BoxFit.fill),
+          deleteIcon: Icon( Icons.close, ),
+          onDeleted: () {setState(() {ddlog("onDeleted:带 avatar 和 deleteIcon的 Chip");}); },
+        ),
+        Divider(),
+
+        Chip(
+          avatar: InkWell(
+            onTap: () { ddlog("onTap:Chip"); },
+            child: Icon(Icons.add_circle,),
+          ),
+          // avatar: IconButton(
+          //   onPressed: () { setState(() { ddlog("add"); } ); },
+          //   padding: EdgeInsets.all(0),
+          //   icon: Icon(Icons.add_circle)) ,
+          label: Text("Chip"),
+          // deleteIcon: Icon(Icons.remove_circle),
+          deleteIcon: Icon(Icons.remove_circle),
+          onDeleted: () { setState(() { ddlog("onDeleted:Chip"); } ); },
+        ),
+        Divider(),
+        RawChip(
+          label: Text("RawChip",),
+          avatar: Image.asset("images/avatar.png", fit: BoxFit.fill),
+          padding: EdgeInsets.all(0),
+          onPressed: (){
+            ddlog("onPressed: RawChip");
+          },
+          deleteIcon: Icon( Icons.close, ),
+          onDeleted: (){
+            ddlog("onDeleted: RawChip");
+          },
+        ),
+        Divider(),
+        RawChip(
+          label: Text("RawChip",),
+          avatar: Icon( Icons.close, ),
+          padding: EdgeInsets.all(0),
+          onPressed: (){
+            ddlog("onPressed: RawChip");
+          },
+        ),
+        Divider(),
+
+
+        Wrap(
+          spacing: 8.0, // 主轴(水平)方向间距
+          runSpacing: -8.0, // 纵轴（垂直）方向间距
+          alignment: WrapAlignment.start, //沿主轴方向居中
+          children: titles.map((e) => ActionChip(
+            avatar: CircleAvatar(
+                backgroundColor: Theme.of(context).primaryColor,
+                child: Text("${e.toString().characters.first.toUpperCase()}")
+            ),
+            label: Text("Action_$e"),
+            onPressed: (){
+              _onPressed(titles.indexOf(e));
+            },
+          )).toList(),
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: titles.map((e) => ChoiceChip(
+            label: Text('Choice_$e'),
+            padding: EdgeInsets.only(left: 15, right: 15),
+            selected: _value == e,
+            onSelected: (bool selected) {
+              ddlog(e);
+              setState(() {
+                _value = selected ? e : null;
+              });
+            },
+          ),).toList(),
+        ),
+        Divider(),
+        Column(
+          children: titles.map((e) => ChoiceChip(
+            label: Text('Choice_$e'),
+            padding: EdgeInsets.only(left: 15, right: 15),
+            selected: _value == e,
+            onSelected: (bool selected) {
+              ddlog(e);
+              setState(() {
+                _value = selected ? e : null;
+              });
+            },
+          ),).toList(),
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ChoiceChip(
+              label: Text('Choice 1'),
+              selected: true,
+            ),
+            ChoiceChip(
+              label: Text('Choice 2'),
+              selected: false,
+            ),
+            ChoiceChip(
+              label: Text('Choice 3'),
+              selected: false,
+            ),
+            ChoiceChip(
+              label: Text('Choice 4'),
+              selected: false,
+            ),
+          ],
+        ),
+        Divider(),
+        InputChip(
+          avatar: CircleAvatar(
+            backgroundColor: Colors.grey.shade800,
+            child: const Text('IC'),
+          ),
+          label: const Text('InputChip'),
+          onPressed: () {
+            print('onPressed: InputChip');
+          }
+        ),
+      ],
+    );
+  }
+
+  void _onPressed(int e) {
+    ddlog(e);
+  }
+}
+
+
+
+class ActorFilterEntry {
+  const ActorFilterEntry(this.name, this.initials);
+  final String name;
+  final String initials;
+}
+
+class ChipFilterDemo extends StatefulWidget {
+  const ChipFilterDemo({Key? key}) : super(key: key);
+
+  @override
+  State createState() => ChipFilterDemoState();
+}
+
+class ChipFilterDemoState extends State<ChipFilterDemo> {
+  final List<ActorFilterEntry> _cast = [
+    const ActorFilterEntry('Aaron Burr', 'AB'),
+    const ActorFilterEntry('Alexander Hamilton', 'AH'),
+    const ActorFilterEntry('Eliza Hamilton', 'EH'),
+    const ActorFilterEntry('James Madison', 'JM'),
+  ];
+  final List<String> _filters = <String>[];
+
+  Iterable<Widget> get actorWidgets sync* {
+    for (final ActorFilterEntry actor in _cast) {
+      yield Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: FilterChip(
+          avatar: CircleAvatar(child: Text(actor.initials)),
+          label: Text(actor.name),
+          selected: _filters.contains(actor.name),
+          onSelected: (bool value) {
+            setState(() {
+              if (value) {
+                _filters.add(actor.name);
+              } else {
+                _filters.removeWhere((String name) {
+                  return name == actor.name;
+                });
+              }
+            });
+          },
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FilterChip'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Wrap(
+            children: actorWidgets.toList(),
+          ),
+          Text('Look for: ${_filters.join(', ')}'),
+        ],
+      ),
+    );
+  }
+}
