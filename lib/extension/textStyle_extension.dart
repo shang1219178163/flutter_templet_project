bf4a553b4351821dfcdede0ea055504e5346865d
Shang <shang1219178163@gmail.com>
2022-11-24 11:57:03 +0800

    代码优化

--- lib/extension/textStyle_extension.dart
+++ lib/extension/textStyle_extension.dart
@@ -1,17 +1,9 @@
 
 
+
 import 'package:flutter/material.dart';
 
-///默认 3D 阴影
-const shadow3D = [
-  const Shadow(
-    offset: Offset(4.0, 4.0),
-    blurRadius: 3.0,
-    color: Color.fromARGB(99, 64, 64, 64),
-  ),
-  const Shadow(
-    offset: Offset(1.0, 1.0),
-    blurRadius: 8.0,
-    color: Colors.grey,
-  ),
-];
+extension TextStyleExt on TextStyle{
+
+
+}
