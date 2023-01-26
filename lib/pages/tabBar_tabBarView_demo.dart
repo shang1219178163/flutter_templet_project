8440b1d57a4870442d3557f1b40e457a748b4c63
Shang <shang1219178163@gmail.com>
2023-01-26 16:21:19 +0800

    更新

--- lib/pages/tabBar_tabBarView_demo.dart
+++ lib/pages/tabBar_tabBarView_demo.dart
@@ -470,7 +470,7 @@ var _customs = [
   Tuple2(APPRouter.customSwipperDemo, "自定义轮播", ),
   Tuple2(APPRouter.neumorphismDemo, "拟物按钮", ),
   Tuple2(APPRouter.horizontalCellDemo, "水平 cell 布局", ),
-  Tuple2(APPRouter.listViewOneDemo, "listViewOneDemo", ),
+  Tuple2(APPRouter.listViewOneDemo, "跑马灯效果", ),
   Tuple2(APPRouter.marqueeWidgetDemo, "跑马灯效果", ),
   Tuple2(APPRouter.ticketDemo, "券", ),
 
