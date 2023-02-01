e209a94b847748eba266609ede43c23dd4df7cb5
shang1219178163 <shang1219178163@gmail.com>
2023-02-01 19:32:47 +0800

    代码重构

--- lib/extension/color_ext.dart
+++ lib/extension/color_ext.dart
@@ -12,7 +12,48 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+
+
 extension ColorExt on Color{
+  /// 颜色字典
+  static final colorsMap = <Color, String>{
+    Colors.white: "white",
+    Colors.black: "black",
+    Colors.pink: "pink",
+    Colors.purple: "purple",
+    Colors.deepPurple: "deepPurple",
+    Colors.indigo: "indigo",
+    Colors.blue: "blue",
+    Colors.lightBlue: "lightBlue",
+    Colors.cyan: "cyan",
+    Colors.teal: "teal",
+    Colors.green: "green",
+    Colors.lightGreen: "lightGreen",
+    Colors.lime: "lime",
+    Colors.yellow: "yellow",
+    Colors.amber: "amber",
+    Colors.orange: "orange",
+    Colors.deepOrange: "deepOrange",
+    Colors.brown: "brown",
+    Colors.grey: "grey",
+    Colors.blueGrey: "blueGrey",
+    Colors.redAccent: "redAccent",
+    Colors.pinkAccent: "pinkAccent",
+    Colors.purpleAccent: "purpleAccent",
+    Colors.deepPurpleAccent: "deepPurpleAccent",
+    Colors.indigoAccent: "indigoAccent",
+    Colors.blueAccent: "blueAccent",
+    Colors.lightBlueAccent: "lightBlueAccent",
+    Colors.cyanAccent: "cyanAccent",
+    Colors.tealAccent: "tealAccent",
+    Colors.greenAccent: "greenAccent",
+    Colors.lightGreenAccent: "lightGreenAccent",
+    Colors.limeAccent: "limeAccent",
+    Colors.yellowAccent: "yellowAccent",
+    Colors.amberAccent: "amberAccent",
+    Colors.orangeAccent: "orangeAccent",
+    Colors.deepOrangeAccent: "deepOrangeAccent",
+  };
 
   ///随机颜色
   static Color get random {
@@ -59,115 +100,11 @@ extension ColorExt on Color{
   Color randomOpacity() {
     return this.withOpacity(Random().nextInt(100)/100);
   }
-
+  
+  /// 颜色名称描述
   String get nameDes{
-    if (this == Colors.white) {
-      return "white";
-    }
-    if (this == Colors.black) {
-      return "black";
-    }
-    if (this == Colors.pink) {
-      return "pink";
-    }
-    if (this == Colors.purple) {
-      return "purple";
-    }
-    if (this == Colors.deepPurple) {
-      return "deepPurple";
-    }
-    if (this == Colors.indigo) {
-      return "indigo";
-    }
-    if (this == Colors.blue) {
-      return "blue";
-    }
-    if (this == Colors.lightBlue) {
-      return "lightBlue";
-    }
-    if (this == Colors.cyan) {
-      return "cyan";
-    }
-    if (this == Colors.teal) {
-      return "teal";
-    }
-    if (this == Colors.green) {
-      return "green";
-    }
-    if (this == Colors.lightGreen) {
-      return "lightGreen";
-    }
-    if (this == Colors.lime) {
-      return "lime";
-    }
-    if (this == Colors.yellow) {
-      return "yellow";
-    }
-    if (this == Colors.amber) {
-      return "amber";
-    }
-    if (this == Colors.orange) {
-      return "orange";
-    }
-    if (this == Colors.deepOrange) {
-      return "deepOrange";
-    }
-    if (this == Colors.brown) {
-      return "brown";
-    }
-    if (this == Colors.grey) {
-      return "grey";
-    }
-    if (this == Colors.blueGrey) {
-      return "blueGrey";
-    }
-    if (this == Colors.redAccent) {
-      return "redAccent";
-    }
-    if (this == Colors.pinkAccent) {
-      return "pinkAccent";
-    }
-    if (this == Colors.purpleAccent) {
-      return "purpleAccent";
-    }
-    if (this == Colors.deepPurpleAccent) {
-      return "deepPurpleAccent";
-    }
-    if (this == Colors.indigoAccent) {
-      return "indigoAccent";
-    }
-    if (this == Colors.blueAccent) {
-      return "blueAccent";
-    }
-    if (this == Colors.lightBlueAccent) {
-      return "lightBlueAccent";
-    }
-    if (this == Colors.cyanAccent) {
-      return "cyanAccent";
-    }
-    if (this == Colors.tealAccent) {
-      return "tealAccent";
-    }
-    if (this == Colors.greenAccent) {
-      return "greenAccent";
-    }
-    if (this == Colors.lightGreenAccent) {
-      return "lightGreenAccent";
-    }
-    if (this == Colors.limeAccent) {
-      return "limeAccent";
-    }
-    if (this == Colors.yellowAccent) {
-      return "yellowAccent";
-    }
-    if (this == Colors.amberAccent) {
-      return "amberAccent";
-    }
-    if (this == Colors.orangeAccent) {
-      return "orangeAccent";
-    }
-    if (this == Colors.deepOrangeAccent) {
-      return "deepOrangeAccent";
+    if (colorsMap.keys.contains(this)) {
+      return colorsMap[this] ?? "";
     }
     return this.toString();
   }
