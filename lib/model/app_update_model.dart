d01715ad6a112ef3564636f6987ed696b48bf980
shang1219178163 <shang1219178163@gmail.com>
2023-03-13 11:16:55 +0800

    更新

--- lib/model/app_update_model.dart
+++ lib/model/app_update_model.dart
@@ -9,14 +9,6 @@
 
 ///升级模型
 class AppUpdateItemModel {
-  final String appIcon;//App图标
-  final String appName;//App名称
-  final String appSize;//App大小
-  final String appDate;//App更新日期
-  final String appDescription;//App更新文案
-  final String appVersion;//App版本
-  final bool isShowAll;//App更新文案
-
 
   const AppUpdateItemModel({
     this.appIcon = "-",
@@ -27,4 +19,118 @@ class AppUpdateItemModel {
     this.appVersion = "-",
     this.isShowAll = false,
   });
+
+  /// App图标
+  final String appIcon;
+  /// App名称
+  final String appName;
+  /// App大小
+  final String appSize;
+  /// App更新日期
+  final String appDate;
+  /// App更新文案
+  final String appDescription;
+  /// App版本
+  final String appVersion;
+  /// App更新文案
+  final bool isShowAll;
+
 }
+
+
+  ///升级模型
+  class AppModel {
+    AppModel({
+      this.appIcon = "-",
+      this.appSize = "-",
+      this.appName = "-",
+      this.appDate = "-",
+      this.appDescription = "-",
+      this.appVersion = "-",
+      this.isShowAll = false,
+    });
+
+    /// App图标
+    String appIcon;
+    /// App名称
+    String appName;
+    /// App大小
+    String appSize;
+    /// App更新日期
+    String appDate;
+    /// App更新文案
+    String appDescription;
+    /// App版本
+    String appVersion;
+    /// App更新文案
+    bool isShowAll;
+
+
+    static AppModel? fromJson(Map<String, Object?>? map) {
+    if (map == null) {
+      return null;
+    }
+    return AppModel(
+      appIcon: map["appIcon"].toString(),
+      appSize: map["appSize"].toString(),
+      appName: map["appName"].toString(),
+      appDate: map["appDate"].toString(),
+      appDescription: map["appDescription"].toString(),
+      appVersion: map["appVersion"].toString(),
+      isShowAll: map["isShowAll"] as bool,
+      );
+    }
+
+
+    Map<String, Object?>toJson() {
+      return {
+        "appIcon": this.appIcon,
+        "appSize": this.appSize,
+        "appName": this.appName,
+        "appDate": this.appDate,
+        "appDescription": this.appDescription,
+        "appVersion": this.appVersion,
+        "isShowAll": this.isShowAll,
+      };
+    }
+
+
+    Object? operator [](String key){
+      final map = this.toJson();
+      final result = map[key];
+      return result;
+    }
+
+
+    void operator []=(String key, dynamic value){
+      switch (key) {
+        case "appName":
+          this.appName = value;
+          break;
+        case "appIcon":
+          this.appIcon = value;
+          break;
+        case "appSize":
+          this.appSize = value;
+          break;
+        case "appName":
+          this.appName = value;
+          break;
+        case "appDate":
+          this.appDate = value;
+          break;
+        case "appDescription":
+          this.appDescription = value;
+          break;
+        case "appVersion":
+          this.appVersion = value;
+          break;
+        case "isShowAll":
+          this.isShowAll = value;
+          break;
+        default:
+          break;
+      }
+    }
+
+  }
