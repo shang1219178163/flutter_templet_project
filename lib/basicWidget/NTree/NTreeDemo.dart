00f099894ec7148cfa2b4ceefca7360501c440c8
Shang <shang1219178163@gmail.com>
2023-07-14 22:13:42 +0800

    更新

--- lib/basicWidget/NTree/NTreeDemo.dart
+++ lib/basicWidget/NTree/NTreeDemo.dart
@@ -8,7 +8,7 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter_templet_project/basicWidget/TreeNode/NTree.dart';
+import 'package:flutter_templet_project/basicWidget/NTree/NTree.dart';
 
 class NTreeDemo extends StatefulWidget {
 
