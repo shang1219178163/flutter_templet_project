2bc57dd3f0ca757130c73dfbf2dd9b4b26107b15
Shang <shang1219178163@gmail.com>
2023-07-23 11:36:35 +0800

    新增文件压缩方法

--- lib/mixin/compress_image_mixin.dart
+++ lib/mixin/compress_image_mixin.dart
@@ -7,16 +7,24 @@ import 'package:flutter/foundation.dart';
 // import 'package:image/image.dart' as compress_image;
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 
+
 /// 压缩
 mixin CompressImageMixin{
+
   /// 压缩图片
   FutureOr<Uint8List> toCompressImage(Uint8List bytes) async {
     return compute(_comporessUint8List, bytes);
   }
 
+
+  /// 压缩文件
+  Future<XFile?> toCompressFile(CompressFileModel model,) async {
+    return compute(_comporessFile, model);
+  }
+
   Future<Uint8List> _comporessUint8List(Uint8List bytes) async {
-    int length = bytes.lengthInBytes;
-    int quality = _getQuality(length);
+    var length = bytes.lengthInBytes;
+    var quality = _getQuality(length);
 
     var result = await FlutterImageCompress.compressWithList(
       bytes,
@@ -28,31 +36,54 @@ mixin CompressImageMixin{
     return result;
   }
 
+  Future<XFile?> _comporessFile(CompressFileModel model,) async {
+    var quality = _getQuality(model.length);
+    final result = await FlutterImageCompress.compressAndGetFile(
+      model.path,
+      model.targetPath,
+      minWidth: 1080,
+      minHeight: 1440,
+      quality: quality,
+      format: CompressFormat.jpeg,
+    );
+    return result;
+  }
+
   int _getQuality(int length) {
-    var quality = 100;
+    // var quality = 100;
     const mb = 1024 * 1024;
     if (length > 10 * mb) {
-      quality = 20;
-    } else if (length > 8 * mb) {
-      quality = 30;
-    } else if (length > 6 * mb) {
-      quality = 40;
-    } else if (length > 4 * mb) {
-      quality = 50;
-    } else if (length > 2 * mb) {
-      quality = 60;
-    } else if (length > 1 * mb) {
-      quality = 70;
-    } else if (length > 0.5 * mb) {
-      quality = 80;
-    } else if (length > 0.25 * mb) {
-      // quality = 90;
+      return 20;
     }
-    return quality;
+
+    if (length > 8 * mb) {
+      return 30;
+    }
+
+    if (length > 6 * mb) {
+      return 40;
+    }
+
+    if (length > 4 * mb) {
+      return 50;
+    }
+
+    if (length > 2 * mb) {
+      return 60;
+    }
+    return 90;
   }
 }
 
 
-
-
-
+/// 文件压缩模型
+class CompressFileModel{
+  CompressFileModel(
+    this.path,
+    this.length,
+    this.targetPath,
+  );
+  String path;
+  int length;
+  String targetPath;
+}
