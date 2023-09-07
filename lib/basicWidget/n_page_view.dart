7d89fe65532c3d72c538376a5e881ec02b450f79
shang1219178163 <shang1219178163@gmail.com>
2023-09-07 19:11:54 +0800

    NPageView 组件封装

--- lib/basicWidget/n_page_view.dart
+++ lib/basicWidget/n_page_view.dart
@@ -7,43 +7,37 @@
 //
 
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_templet_project/basicWidget/list_subtitle_cell.dart';
-import 'package:flutter_templet_project/extension/color_ext.dart';
-import 'package:flutter_templet_project/extension/divider_ext.dart';
-import 'package:flutter_templet_project/extension/list_ext.dart';
-import 'package:flutter_templet_project/extension/string_ext.dart';
-import 'package:flutter_templet_project/main.dart';
-import 'package:flutter_templet_project/basicWidget/app_update_card.dart';
-import 'package:flutter_templet_project/extension/ddlog.dart';
-import 'package:flutter_templet_project/basicWidget/section_list_view.dart';
-import 'package:flutter_templet_project/model/mock_data.dart';
-import 'package:flutter_templet_project/pages/tabBar_tabBarView_demo.dart';
-import 'package:flutter_templet_project/routes/APPRouter.dart';
-
-import 'package:get/get.dart';
+import 'package:flutter_templet_project/extension/build_context_ext.dart';
 import 'package:tuple/tuple.dart';
-
-import 'package:flutter_templet_project/extension/widget_ext.dart';
 
 
 class NPageView extends StatefulWidget {
 
-  final String? title;
 
-  const NPageView({ Key? key, this.title}) : super(key: key);
+  const NPageView({
+    Key? key,
+    required this.items,
+    this.hideAppbar = false,
+    this.isTabBar = false,
+  }) : super(key: key);
 
-  
+  final List<Tuple2<String, Widget>> items;
+
+  final bool hideAppbar;
+
+  final bool isTabBar;
+
   @override
   _NPageViewState createState() => _NPageViewState();
 }
 
 class _NPageViewState extends State<NPageView> with SingleTickerProviderStateMixin {
-  late final _tabController = TabController(length: items.length, vsync: this);
+
+  late final _tabController = TabController(length: widget.items.length, vsync: this);
 
   late final _pageController = PageController(initialPage: 0, keepPage: true);
 
-  var isTabBar = ValueNotifier(false);
+  late final _isTabBarVN = ValueNotifier(widget.isTabBar);
 
   @override
   void initState() {
@@ -53,57 +47,26 @@ class _NPageViewState extends State<NPageView> with SingleTickerProviderStateMix
   @override
   void dispose() {
     _tabController.dispose();
+    _pageController.dispose();
     super.dispose();
   }
 
   @override
+  void didUpdateWidget(covariant NPageView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.isTabBar == oldWidget.isTabBar) {
+      return;
+    }
+    _isTabBarVN.value = widget.isTabBar;
+    debugPrint("didUpdateWidget isTabBarVN:${widget.isTabBar}, _isTabBarVN:${_isTabBarVN.value}");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('页面封装'),
-        actions: [
-          TextButton(
-            onPressed: (){
-              isTabBar.value = !isTabBar.value;
-              setState(() {});
-            },
-            child: Text(isTabBar.value ? "top" : "bottom", style: TextStyle(color: Colors.white),),
-          ),
-        ],
-        bottom: isTabBar.value ? null : _buildTabBar(),
-      ),
-      body: _buildPageView(),
-      bottomNavigationBar: !isTabBar.value ? null : _buildBottomNavigationBar(),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        onPressed: () {
-
-        },
-        child: Icon(Icons.add),
-      ),
-      // persistentFooterButtons: persistentFooterButtons(),
-    );
+    return _buildPageView(items: widget.items);
   }
 
-  PreferredSize _buildPreferredSize(Widget child) {
-    return PreferredSize(
-      preferredSize: Size.fromHeight(48),
-      child: Theme(
-        data: Theme.of(context).
-          copyWith(colorScheme: ColorScheme.fromSwatch().
-          copyWith(secondary: Colors.white)
-        ),
-        child: Container(
-          height: 40,
-          alignment: Alignment.center, //圆点居中
-          //给自定义导航栏设置圆点控制器
-          child: child,
-        ),
-      ),
-    );
-  }
-
-  TabBar _buildTabBar() {
+  TabBar _buildTabBar({required List<Tuple2<String, Widget>> items,}) {
     return TabBar(
       controller: _tabController,
       isScrollable: true,
@@ -111,18 +74,15 @@ class _NPageViewState extends State<NPageView> with SingleTickerProviderStateMix
       indicatorSize: TabBarIndicatorSize.label,
       // indicatorPadding: EdgeInsets.only(left: 6, right: 6),
       onTap: (index){
-        setState(() {
-          _pageController.jumpToPage(index);
-        });
+        _pageController.jumpToPage(index);
+        setState(() {});
       },
     );
   }
-  Material _buildBottomNavigationBar() {
+
+  Material _buildBottomNavigationBar({required List<Tuple2<String, Widget>> items,}) {
     final textColor = Theme.of(context).colorScheme.secondary;
     const bgColor = Colors.white;
-
-    // final bgColor = Theme.of(context).colorScheme.secondary;
-    // final textColor = Colors.white;
 
     return Material(
       color: bgColor,
@@ -134,150 +94,63 @@ class _NPageViewState extends State<NPageView> with SingleTickerProviderStateMix
           indicator: BoxDecoration(
             border: Border(
               top: BorderSide(
-                  // color: textColor ,
                   color: textColor,
                   width: 3.0
               ),
             ),
           ),
           onTap: (index){
-            ddlog(index);
-            setState(() {
-              // _tabController.animateTo(index);
-              _pageController.jumpToPage(index);
-            });
+            _pageController.jumpToPage(index);
+            setState(() {});
           },
         ),
       )
     );
   }
 
-
-  Widget _buildPageView() {
-    return PageView(
-      controller: _pageController,
-      onPageChanged: (index) {
-        _tabController.animateTo(index);
-        setState(() {});
-      },
-      // children: _pages.map((e) => Tab(text: e.item1)).toList(),
-      children: items.map((e) => e.item2).toList(),
+  Widget _buildPageView({required List<Tuple2<String, Widget>> items,}) {
+    return Column(
+      children: [
+        ValueListenableBuilder(
+           valueListenable: _isTabBarVN,
+           builder: (context,  value, child){
+            if (value) {
+              return SizedBox();
+            }
+            return Container(
+              width: double.maxFinite,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: primaryColor,
+              ),
+              constraints: BoxConstraints(maxHeight: 48.0),
+              child: _buildTabBar(items: items),
+            );
+          }
+        ),
+        Expanded(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (index) {
+              _tabController.animateTo(index);
+              setState(() {});
+            },
+            // children: _pages.map((e) => Tab(text: e.item1)).toList(),
+            children: items.map((e) => e.item2).toList(),
+          ),
+        ),
+        ValueListenableBuilder(
+            valueListenable: _isTabBarVN,
+            builder: (context,  value, child){
+              if (!value) {
+                return SizedBox();
+              }
+              return _buildBottomNavigationBar(items: items);
+            }
+        ),
+      ],
     );
   }
 
 
-  List<Widget> persistentFooterButtons() {
-    return [
-      TextButton(
-        onPressed: () => setState(() => debugPrint('button1 tapped')),
-        child: Text(
-          'Button 1',
-        ),
-      ),
-      IconButton(
-        icon: Icon(Icons.map),
-        onPressed: () => setState(() => debugPrint('button1 tapped')),
-      ),
-      IconButton(
-        icon: Icon(Icons.mail),
-        onPressed: () => setState(() => debugPrint('button1 tapped')),
-      ),
-    ];
-  }
-
-  List<Tuple2<String, Widget>> items = [
-    Tuple2('功能列表', ListView.separated(
-      cacheExtent: 180,
-      itemCount: kAliPayList.length,
-      itemBuilder: (context, index) {
-        final data = kAliPayList[index];
-        return ListSubtitleCell(
-          padding: EdgeInsets.all(10),
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: Image.network(
-              data.imageUrl,
-              width: 40,
-              height: 40,
-            ),
-          ),
-          title: Text(
-            data.title,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-              color: Color(0xFF333333),
-            ),
-          ),
-          subtitle: Text(data.content,
-            // maxLines: 1,
-            // overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: 15,
-              color: Color(0xFF999999),
-            ),
-          ),
-          trailing: Text(data.time,
-            style: TextStyle(
-              fontSize: 13,
-              color: Color(0xFF999999),
-            ),
-          ),
-          subtrailing: Text("已完成",
-            style: TextStyle(
-              fontSize: 13,
-              color: Colors.blue,
-            ),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) {
-        return DividerExt.custome();
-      },
-    )),
-
-    Tuple2('升级列表(新)', ListView.separated(
-      cacheExtent: 180,
-      itemCount: kUpdateAppList.length,
-      itemBuilder: (context, index) {
-        final data = kUpdateAppList[index];
-        if (index == 0) {
-          return AppUpdateCard(data: data, isExpand: true, showExpand: false,);
-        }
-        return AppUpdateCard(data: data);
-      },
-      separatorBuilder: (context, index) {
-        return Divider();
-      },
-    )),
-
-    Tuple2('列表(泛型)', SectionListView<String, Tuple2<String, String>>(
-      headerList: tuples.map((e) => e.item1).toList(),
-      itemList: tuples.map((e) => e.item2).toList()
-          .map((e) => e.sorted((a, b) => a.item1.toLowerCase().compareTo(b.item1.toLowerCase()))).toList(),
-      headerBuilder: (e) {
-        return Container(
-          // color: Colors.red,
-          padding: EdgeInsets.only(top: 10, bottom: 8, left: 10, right: 15),
-          child: Text(e, style: TextStyle(fontWeight: FontWeight.w600),),
-        );
-      },
-      itemBuilder: (section, row, e) {
-        return ListTile(
-          title: Text(e.item2),
-          subtitle: Text(e.item2.toCapitalize()),
-          trailing: Icon(Icons.keyboard_arrow_right_rounded),
-          dense: true,
-          onTap: (){
-            Get.toNamed(e.item1, arguments: e);
-            if (e.item1.toLowerCase().contains("loginPage".toLowerCase())){
-              Get.offNamed(e.item1, arguments: e.item1);
-            } else {
-              Get.toNamed(e.item1, arguments: e.item1);
-            }
-          },
-        );
-      },
-    ),),
-  ];
 }
--- lib/pages/demo/NPageViewDemo.dart
+++ lib/pages/demo/NPageViewDemo.dart
@@ -1,7 +1,18 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/basicWidget/app_update_card.dart';
+import 'package:flutter_templet_project/basicWidget/list_subtitle_cell.dart';
 import 'package:flutter_templet_project/basicWidget/n_page_view.dart';
+import 'package:flutter_templet_project/basicWidget/section_list_view.dart';
+import 'package:flutter_templet_project/extension/divider_ext.dart';
+import 'package:flutter_templet_project/extension/list_ext.dart';
+import 'package:flutter_templet_project/extension/string_ext.dart';
+import 'package:flutter_templet_project/model/mock_data.dart';
+import 'package:flutter_templet_project/pages/tabBar_tabBarView_demo.dart';
+import 'package:get/get.dart';
+import 'package:tuple/tuple.dart';
+
 
 class NPageViewDemo extends StatefulWidget {
 
@@ -18,27 +29,140 @@ class NPageViewDemo extends StatefulWidget {
 
 class _NPageViewDemoState extends State<NPageViewDemo> {
 
+  late var isTabBarVN = ValueNotifier(false);
+
 
   @override
   Widget build(BuildContext context) {
-    dynamic arguments = ModalRoute.of(context)!.settings.arguments;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title ?? "$widget"),
-        actions: ['done',].map((e) => TextButton(
+        actions: ['change',].map((e) => TextButton(
           child: Text(e,
             style: TextStyle(color: Colors.white),
           ),
-          onPressed: () => debugPrint(e),)
+          onPressed: () {
+            isTabBarVN.value = !isTabBarVN.value;
+            debugPrint("isTabBarVN:${isTabBarVN.value}");
+          },
+        )
         ).toList(),
+        elevation: 0,
       ),
       body: buildBody()
     );
   }
 
   buildBody() {
-    return NPageView();
+    return ValueListenableBuilder(
+       valueListenable: isTabBarVN,
+       builder: (context,  value, child){
+
+        return NPageView(
+          items: items,
+          // hideAppbar: true,
+          // isTabBar: isTabBarVN.value,
+          isTabBar: value,
+        );
+      }
+    );
   }
 
+
+  List<Tuple2<String, Widget>> items = [
+    Tuple2('功能列表', ListView.separated(
+      cacheExtent: 180,
+      itemCount: kAliPayList.length,
+      itemBuilder: (context, index) {
+
+        final data = kAliPayList[index];
+        return ListSubtitleCell(
+          padding: EdgeInsets.all(10),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Image.network(
+              data.imageUrl,
+              width: 40,
+              height: 40,
+            ),
+          ),
+          title: Text(
+            data.title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Color(0xFF333333),
+            ),
+          ),
+          subtitle: Text(data.content,
+            // maxLines: 1,
+            // overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 15,
+              color: Color(0xFF999999),
+            ),
+          ),
+          trailing: Text(data.time,
+            style: TextStyle(
+              fontSize: 13,
+              color: Color(0xFF999999),
+            ),
+          ),
+          subtrailing: Text("已完成",
+            style: TextStyle(
+              fontSize: 13,
+              color: Colors.blue,
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return DividerExt.custome();
+      },
+    )),
+
+    Tuple2('升级列表(新)', ListView.separated(
+      cacheExtent: 180,
+      itemCount: kUpdateAppList.length,
+      itemBuilder: (context, index) {
+        final data = kUpdateAppList[index];
+        if (index == 0) {
+          return AppUpdateCard(data: data, isExpand: true, showExpand: false,);
+        }
+        return AppUpdateCard(data: data);
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    )),
+
+    Tuple2('列表(泛型)', SectionListView<String, Tuple2<String, String>>(
+      headerList: tuples.map((e) => e.item1).toList(),
+      itemList: tuples.map((e) => e.item2).toList()
+          .map((e) => e.sorted((a, b) => a.item1.toLowerCase().compareTo(b.item1.toLowerCase()))).toList(),
+      headerBuilder: (e) {
+        return Container(
+          // color: Colors.red,
+          padding: EdgeInsets.only(top: 10, bottom: 8, left: 10, right: 15),
+          child: Text(e, style: TextStyle(fontWeight: FontWeight.w600),),
+        );
+      },
+      itemBuilder: (section, row, e) {
+        return ListTile(
+          title: Text(e.item2),
+          subtitle: Text(e.item2.toCapitalize()),
+          trailing: Icon(Icons.keyboard_arrow_right_rounded),
+          dense: true,
+          onTap: (){
+            Get.toNamed(e.item1, arguments: e);
+            if (e.item1.toLowerCase().contains("loginPage".toLowerCase())){
+              Get.offNamed(e.item1, arguments: e.item1);
+            } else {
+              Get.toNamed(e.item1, arguments: e.item1);
+            }
+          },
+        );
+      },
+    ),),
+  ];
 }
