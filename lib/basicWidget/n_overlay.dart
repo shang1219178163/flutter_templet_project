24d4538ecc9dffca38d41e23e369d8573ac14272
shang1219178163 <shang1219178163@gmail.com>
2023-09-08 19:14:43 +0800

    修改名称

--- lib/basicWidget/n_overlay.dart
+++ lib/basicWidget/n_overlay.dart
@@ -1,11 +1,22 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_templet_project/basicWidget/n_adaptive_text.dart';
 import 'package:flutter_templet_project/basicWidget/n_slide_transition_builder.dart';
+import 'package:flutter_templet_project/basicWidget/n_text.dart';
 import 'package:flutter_templet_project/extension/overlay_ext.dart';
+import 'package:flutter_templet_project/uti/app_uti.dart';
+import 'package:get/get.dart';
 
-class NToast{
+
+/// 浮层工具类
+class NOverlay{
+
+  static final _globalContext = Get.context;
+
+  static final _overlayState = Overlay.of(_globalContext!);
+
   /// overlay 层集合
   static final List<OverlayEntry> _entriesList = [];
   /// 当前弹窗
@@ -22,14 +33,18 @@ class NToast{
   /// OverlayEntry 弹窗展示
   static OverlayEntry? showEntry({
     required BuildContext context,
-    required Widget child,
     bool isReplace = false,
     bool maintainState = false,
+    required Widget child,
   }){
+    // var context = AppUtil.globalContext;
     final OverlayState? _overlayState = Overlay.of(context);
     if (_overlayState == null) {
       return null;
     }
+
+    // debugPrint("AppUtil.globalContext: ${_globalContext}");
+    // final _overlayState = Overlay.of(_globalContext!);
 
     if (isReplace) {
       hideEntry();
@@ -66,17 +81,18 @@ class NToast{
   /// 展示 OverlayEntry 弹窗
   static showToast({
     required BuildContext context,
-    String text = "showToast",
+    String message = "showToast",
     Widget? child,
     Alignment alignment = Alignment.center,
     Duration duration = const Duration(milliseconds: 2000),
-    bool barrierDismissible = true,
+    bool autoDismiss = true,
     Color? barrierColor = Colors.transparent,
     VoidCallback? onBarrier,
+    VoidCallback? onDismiss,
     bool isLast = false,
   }) {
     Widget content = NAdaptiveText(
-      data: text,
+      data: message,
       alignment: alignment,
       child: child,
     );
@@ -99,11 +115,54 @@ class NToast{
         ],
       );
     }
-    showEntry(context: context, child: content);
+    showEntry(
+      context: context,
+      child: content,
+    );
 
-    if (barrierDismissible) {
-      Future.delayed(duration, hideEntry);
+    if (autoDismiss) {
+      Future.delayed(duration, (){
+        hideEntry();
+        onDismiss?.call();
+      });
     }
+  }
+
+
+  static showLoading({
+    required BuildContext context,
+    String message = "showToast",
+    Widget? child,
+  }) {
+    NOverlay.showToast(
+      context: context,
+      duration: const Duration(milliseconds: 5000),
+      autoDismiss: true,
+      onDismiss: (){
+        debugPrint("onDismiss: ${DateTime.now()}");
+      },
+      // barrierDismissible: false,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                _SpinkitLoading(),
+              ],
+            ),
+          ),
+          child ?? NText(data: 'NToast is awesome!',
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
   }
 
   /// 滑进滑出弹窗
@@ -126,23 +185,65 @@ class NToast{
     }
 
     showEntry(
-        context: context,
-        isReplace: isReplace,
-        maintainState: maintainState,
-        child: Material(
-          color: Colors.black.withOpacity(0.1),
-          child: InkWell(
-            onTap: !barrierDismissible ? null : onHide,
-            child: NSlideTransitionBuilder(
-              key: globalKey,
-              alignment: alignment,
-              duration: duration,
-              hasFade: false,
-              child: builder(context, onHide),
-            ),
+      context: context,
+      isReplace: isReplace,
+      maintainState: maintainState,
+      child: Material(
+        color: Colors.black.withOpacity(0.1),
+        child: InkWell(
+          onTap: !barrierDismissible ? null : onHide,
+          child: NSlideTransitionBuilder(
+            key: globalKey,
+            alignment: alignment,
+            duration: duration,
+            hasFade: false,
+            child: builder(context, onHide),
           ),
-        )
+        ),
+      )
     );
+  }
+
+
+
+
+}
+
+
+
+class _SpinkitLoading extends StatefulWidget {
+
+  _SpinkitLoading({
+    Key? key,
+    this.message = "",
+  }) : super(key: key);
+
+  final String message;
+
+  @override
+  _SpinkitLoadingState createState() => _SpinkitLoadingState();
+}
+
+class _SpinkitLoadingState extends State<_SpinkitLoading> with SingleTickerProviderStateMixin {
+
+  final spinkit = SpinKitFadingCircle(
+    color: Colors.white,
+    // itemBuilder: (BuildContext context, int index) {
+    //   return DecoratedBox(
+    //     decoration: BoxDecoration(
+    //       color: index.isEven ? Colors.red : Colors.green,
+    //     ),
+    //   );
+    // },
+  );
+
+  final spinkit1 = SpinKitCircle(
+    color: Colors.white,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return spinkit1;
   }
 
 }
