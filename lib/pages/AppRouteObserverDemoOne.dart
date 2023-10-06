5424d8cfd89e49fda8bac66499831fa2c5ebb049
shang1219178163 <shang1219178163@gmail.com>
2023-10-06 15:07:49 +0800

    新增文件

---
+++ lib/pages/AppRouteObserverDemoOne.dart
@@ -0,0 +1,90 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/routes/AppRouteObserver.dart';
+import 'package:flutter_templet_project/routes/AppRouter.dart';
+import 'package:get/get.dart';
+
+/// 路由监听
+class AppRouteObserverDemoOne extends StatefulWidget {
+
+  AppRouteObserverDemoOne({
+    Key? key,
+    this.title
+  }) : super(key: key);
+
+  final String? title;
+
+  @override
+  _AppRouteObserverDemoOneState createState() => _AppRouteObserverDemoOneState();
+}
+
+class _AppRouteObserverDemoOneState extends State<AppRouteObserverDemoOne> with RouteAware, RouteAwareMixin {
+
+  @override
+  void dispose() {
+    debugPrint("------> $widget dispose");
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    debugPrint("------> $widget initState");
+    super.initState();
+  }
+
+
+  @override
+  void didPush() {
+    debugPrint("------> $widget didPush");
+    super.didPush();
+  }
+
+  @override
+  void didPop() {
+    debugPrint("------> $widget didPop");
+    super.didPop();
+  }
+
+  @override
+  void didPushNext() {
+    debugPrint("------> $widget didPushNext");
+    super.didPushNext();
+  }
+
+  @override
+  void didPopNext() {
+    debugPrint("------> $widget didPopNext");
+    super.didPopNext();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? "$widget"),
+        actions: ['done',].map((e) => TextButton(
+          child: Text(e,
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () => debugPrint(e),)
+        ).toList(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Get.toNamed(APPRouter.alertSheetDemo);
+              },
+              child: Text("next"),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
+
+
+
---
+++ lib/pages/demo/ContextMenuDemo.dart
@@ -0,0 +1,108 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ContextMenuDemo extends StatefulWidget {
+
+  ContextMenuDemo({
+    Key? key, 
+    this.title
+  }) : super(key: key);
+
+  final String? title;
+
+  @override
+  _ContextMenuDemoState createState() => _ContextMenuDemoState();
+}
+
+class _ContextMenuDemoState extends State<ContextMenuDemo> {
+  static const String route = 'anywhere';
+  static const String title = 'Context Menu Anywhere Example';
+  static const String subtitle = 'A context menu outside of a text field';
+
+  // final PlatformCallback onChangedPlatform;
+
+  final TextEditingController _materialController = TextEditingController(
+    text: 'TextField shows the default menu still.',
+  );
+  final TextEditingController _cupertinoController = TextEditingController(
+    text: 'CupertinoTextField shows the default menu still.',
+  );
+  final TextEditingController _editableController = TextEditingController(
+    text: 'EditableText has no default menu, so it shows the custom one.',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$this"),
+        actions: <Widget>[
+          // PlatformSelector(
+          //   onChangedPlatform: onChangedPlatform,
+          // ),
+          // IconButton(
+          //   icon: const Icon(Icons.code),
+          //   onPressed: () async {
+          //     if (!await launchUrl(Uri.parse(url))) {
+          //       throw 'Could not launch $url';
+          //     }
+          //   },
+          // ),
+        ],
+      ),
+      // body: ContextMenuRegion(
+      //   contextMenuBuilder: (context, primaryAnchor, [secondaryAnchor]) {
+      //     return AdaptiveTextSelectionToolbar.buttonItems(
+      //       anchors: TextSelectionToolbarAnchors(
+      //         primaryAnchor: primaryAnchor,
+      //         secondaryAnchor: secondaryAnchor as Offset?,
+      //       ),
+      //       buttonItems: <ContextMenuButtonItem>[
+      //         ContextMenuButtonItem(
+      //           onPressed: () {
+      //             ContextMenuController.removeAny();
+      //             Navigator.of(context).pop();
+      //           },
+      //           label: 'Back',
+      //         ),
+      //       ],
+      //     );
+      //   },
+      //   child: buildChild(),
+      // ),
+    );
+  }
+
+  buildChild() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 64.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(height: 20.0),
+          const Text(
+            'Right click anywhere outside of a field to show a custom menu.',
+          ),
+          Container(height: 140.0),
+          CupertinoTextField(controller: _cupertinoController),
+          Container(height: 40.0),
+          TextField(controller: _materialController),
+          Container(height: 40.0),
+          Container(
+            color: Colors.white,
+            child: EditableText(
+              controller: _editableController,
+              focusNode: FocusNode(),
+              style: Typography.material2021().black.displayMedium!,
+              cursorColor: Colors.blue,
+              backgroundCursorColor: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
