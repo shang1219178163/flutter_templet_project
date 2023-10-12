d4df1376db44a5a38fd33734fd4e3c9c2867af23
shang1219178163 <shang1219178163@gmail.com>
2023-10-12 14:08:20 +0800

    新增

---
+++ lib/vendor/FlutterIsolateDemo.dart
@@ -0,0 +1,71 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_isolate/flutter_isolate.dart';
+
+class FlutterIsolateDemo extends StatefulWidget {
+
+  FlutterIsolateDemo({
+    Key? key, 
+    this.title
+  }) : super(key: key);
+
+  final String? title;
+
+  @override
+  _FlutterIsolateDemoState createState() => _FlutterIsolateDemoState();
+}
+
+class _FlutterIsolateDemoState extends State<FlutterIsolateDemo> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic arguments = ModalRoute.of(context)!.settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? "$widget"),
+        actions: ['done',].map((e) => TextButton(
+          child: Text(e,
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () => debugPrint(e),)
+        ).toList(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('01 - 简单的执行顶层函数'),
+              onPressed: () {
+                FlutterIsolate.spawn(someFunction, "hello world");
+              },
+            ),
+
+            ElevatedButton(
+              child: const Text('02 - Compute函数'),
+              onPressed: () async {
+                var res = await flutterCompute(expensiveWork, 123);
+                debugPrint(res.toString());
+              },
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
+
+  @pragma('vm:entry-point')
+  void someFunction(String arg) {
+    debugPrint("Running in an isolate with argument : $arg");
+  }
+
+
+  @pragma('vm:entry-point')
+  Future<int> expensiveWork(int arg) async {
+    int result = arg + 100;
+    return result;
+  }
+}
