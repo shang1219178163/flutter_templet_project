deffe8a937ccf4a3341aa5368dac6d90856ed531
shang1219178163 <shang1219178163@gmail.com>
2024-01-12 20:41:26 +0800

    去冗余

--- lib/extension/date_time_ext.dart
+++ lib/extension/date_time_ext.dart
@@ -217,11 +217,11 @@ extension DateTimeIntExt on int{
 // print('Milliseconds: ${duration.inMillisecondsRest}'); // 0
 // print('Microseconds: ${duration.inMicrosecondsRest}'); // 0
 
-extension DurationExt on Duration {
-  int get inDaysRest => inDays;
-  int get inHoursRest => inHours - (inDays * 24);
-  int get inMinutesRest => inMinutes - (inHours * 60);
-  int get inSecondsRest => inSeconds - (inMinutes * 60);
-  int get inMillisecondsRest => inMilliseconds - (inSeconds * 1000);
-  int get inMicrosecondsRest => inMicroseconds - (inMilliseconds * 1000);
-}
+// extension DurationExt on Duration {
+//   int get inDaysRest => inDays;
+//   int get inHoursRest => inHours - (inDays * 24);
+//   int get inMinutesRest => inMinutes - (inHours * 60);
+//   int get inSecondsRest => inSeconds - (inMinutes * 60);
+//   int get inMillisecondsRest => inMilliseconds - (inSeconds * 1000);
+//   int get inMicrosecondsRest => inMicroseconds - (inMilliseconds * 1000);
+// }
