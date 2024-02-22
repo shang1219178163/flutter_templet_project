7e8de5a8d06ddfb70a8fc4725c284510755c9d4c
shang1219178163 <shang1219178163@gmail.com>
2024-02-22 14:42:48 +0800

    重写方法,避免页面退出之后调用导致的问题

---
+++ lib/mixin/safe_set_state_mixin.dart
@@ -0,0 +1,23 @@
+//
+//  SafeSetStateMixin.dart
+//  flutter_templet_project
+//
+//  Created by shang on 2024/2/22 14:26.
+//  Copyright © 2024/2/22 shang. All rights reserved.
+//
+
+import 'package:flutter/material.dart';
+
+/// 重写方法,避免页面退出之后方法 调用导致的问题
+mixin SafeSetStateMixin<T extends StatefulWidget> on State<T> {
+
+  /// 重写方法,避免页面退出之后调用导致的问题
+  @override
+  void setState(VoidCallback fn) {
+    if (!mounted) {
+      return;
+    }
+    super.setState(fn);
+  }
+
+}
---
+++ lib/vendor/getx/order_controller.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+/// 订单逻辑类
+class OrderController extends GetxController {
+
+  /// 重写方法,避免页面退出之后的 update 调用导致的问题
+  @override
+  void update([List<Object>? ids, bool condition = true]) {
+    if (!Get.isRegistered<OrderController>()) {
+      return;
+    }
+    super.update();
+  }
+
+  /// 详情
+  requestDetail() async {
+    await Future.delayed(Duration(milliseconds: 1500));
+    update();
+  }
+
+  /// 提交
+  submit({required Map map,}) async {
+    await Future.delayed(Duration(milliseconds: 1500));
+    update();
+  }
+}
