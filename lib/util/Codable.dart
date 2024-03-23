a907ee2b0aecefdab4a219c633e42547ba5e0f04
Shang <shang1219178163@gmail.com>
2024-03-23 11:45:13 +0800

    编码协议探究

--- lib/util/Codable.dart
+++ lib/util/Codable.dart
@@ -6,11 +6,40 @@ abstract class Codable {
   //   throw UnimplementedError();
   // }
 
-  factory Codable.fromJson(Map<String, dynamic>? json) {
+  // Codable.fromJson(Map<String, dynamic>? json) {
+  //   throw UnimplementedError();
+  // }
+
+  factory Codable.fromJson(Map<String, dynamic> json) {
     throw UnimplementedError();
   }
 
   Map<String, dynamic> toJson() {
     throw UnimplementedError();
   }
+}
+
+
+class XYZModel implements Codable{
+
+  XYZModel({this.id, this.name,});
+  String? id;
+  String? name;
+
+  XYZModel.fromJson(Map<String, dynamic>? json) {
+    if (json == null) {
+      return;
+    }
+
+    id = json["id"];
+    name = json["name"];
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    // TODO: implement toJson
+    throw UnimplementedError();
+  }
+
+
 }
