85d5350d533b144c2306e2c0608b7b0bd414ec37
Shang <shang1219178163@gmail.com>
2024-04-03 15:44:53 +0800

    请求方法二次封装

--- lib/network/base_request_api.dart
+++ lib/network/base_request_api.dart
@@ -102,6 +102,26 @@ class BaseRequestAPI {
     return response;
   }
 
+  /// 数据请求
+  ///
+  /// onResult 根据 response 返回和泛型 T 对应的值(默认值取 response["result"])
+  ///
+  /// return (请求是否成功, 提示语)
+  Future<({bool isSuccess, String message, T? result})>fetchResult<T>({
+    required T Function(Map<String, dynamic> response)? onResult,
+  }) async {
+    BaseRequestAPI api = this;
+    final response = await api.fetch();
+    if (response.isEmpty) {
+      return (isSuccess: false, message: "", result: null);//断网
+    }
+    bool isSuccess = response["code"] == "OK";
+    String message = response["message"] ?? "";
+    final result = response["result"] as T?;
+    final resultNew = onResult?.call(response) ?? result;
+    return (isSuccess: isSuccess, message: message, result: resultNew);
+  }
+
   /// 返回布尔值的数据请求
   ///
   /// onSuccess 根据字典返回 true 的判断条件(默认判断 response["result"] 布尔值真假)
@@ -112,24 +132,13 @@ class BaseRequestAPI {
     bool Function(Map<String, dynamic> response)? onSuccess,
     bool hasLoading = true,
   }) async {
-    BaseRequestAPI api = this;
-    if (hasLoading) {
-      EasyToast.showLoading("请求中");
-    }
-    final response = await api.fetch();
-    if (hasLoading) {
-      EasyToast.hideLoading();
-    }
-    if (response.isEmpty) {
-      return (isSuccess: false, message: "");
-    }
-
-    String message = response["message"] ?? "";
-    final result = (response["result"] as bool?) ?? false;
-    final resultNew = onSuccess?.call(response) ?? result;
-
-    bool isSuccess = response["code"] == "OK" && resultNew;
-    return (isSuccess: isSuccess , message: message);
+    final tuple = await fetchResult<bool>(
+      onResult: (response) {
+        final result = onSuccess?.call(response) ?? response["result"] ?? false;
+        return result;
+      },
+    );
+    return (isSuccess: tuple.isSuccess, message: tuple.message);
   }
 
   /// 返回列表类型请求接口
@@ -141,17 +150,66 @@ class BaseRequestAPI {
       fetchList<T extends Map<String, dynamic>>({
     List<T> Function(Map<String, dynamic> response)? onList,
   }) async {
-    BaseRequestAPI api = this;
-    final response = await api.fetch();
-    if (response.isEmpty) {
-      return (isSuccess: false, message: "", list: <T>[]);
-    }
-    bool isSuccess = response["code"] == "OK";
-    String message = response["message"] ?? "";
-    final result = (response["result"] as List<T>?) ?? <T>[];
-    final items = onList?.call(response) ?? result;
-    return (isSuccess: isSuccess, message: message, list: items);
+    final tuple = await fetchResult<List<T>>(
+      onResult: (response) {
+        final result = onList?.call(response) ?? response["result"] as List<T>? ?? <T>[];
+        return result;
+      },
+    );
+    final list = tuple.result ?? <T>[];
+    return (isSuccess: tuple.isSuccess, message: tuple.message, list: list);
   }
+
+  // /// 返回布尔值的数据请求
+  // ///
+  // /// onSuccess 根据字典返回 true 的判断条件(默认判断 response["result"] 布尔值真假)
+  // /// hasLoading 是否展示 loading
+  // ///
+  // /// return (请求是否成功, 提示语)
+  // Future<({bool isSuccess, String message})> fetchBool({
+  //   bool Function(Map<String, dynamic> response)? onSuccess,
+  //   bool hasLoading = true,
+  // }) async {
+  //   BaseRequestAPI api = this;
+  //   if (hasLoading) {
+  //     EasyToast.showLoading("请求中");
+  //   }
+  //   final response = await api.fetch();
+  //   if (hasLoading) {
+  //     EasyToast.hideLoading();
+  //   }
+  //   if (response.isEmpty) {
+  //     return (isSuccess: false, message: "");
+  //   }
+  //
+  //   String message = response["message"] ?? "";
+  //   final result = (response["result"] as bool?) ?? false;
+  //   final resultNew = onSuccess?.call(response) ?? result;
+  //
+  //   bool isSuccess = response["code"] == "OK" && resultNew;
+  //   return (isSuccess: isSuccess , message: message);
+  // }
+  //
+  // /// 返回列表类型请求接口
+  // ///
+  // /// onList 根据字典返回数组;(默认取 response["result"] 对应的数组值)
+  // ///
+  // /// return (请求是否成功, 提示语, 数组)
+  // Future<({bool isSuccess, String message, List<T> list})>
+  //     fetchList<T extends Map<String, dynamic>>({
+  //   List<T> Function(Map<String, dynamic> response)? onList,
+  // }) async {
+  //   BaseRequestAPI api = this;
+  //   final response = await api.fetch();
+  //   if (response.isEmpty) {
+  //     return (isSuccess: false, message: "", list: <T>[]);
+  //   }
+  //   bool isSuccess = response["code"] == "OK";
+  //   String message = response["message"] ?? "";
+  //   final result = (response["result"] as List<T>?) ?? <T>[];
+  //   final items = onList?.call(response) ?? result;
+  //   return (isSuccess: isSuccess, message: message, list: items);
+  // }
 
 }
 
