6566e98464e14aead35cf94192498bbeaf4f31bc
Shang <shang1219178163@gmail.com>
2024-04-03 14:38:29 +0800

    函数扩展

--- lib/network/base_request_api.dart
+++ lib/network/base_request_api.dart
@@ -2,7 +2,10 @@
 
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter_templet_project/network/RequestConfig.dart';
 import 'package:flutter_templet_project/network/RequestError.dart';
+import 'package:flutter_templet_project/network/RequestManager.dart';
+import 'package:flutter_templet_project/vendor/easy_toast.dart';
 
 
 enum HttpMethod {
@@ -92,7 +95,62 @@ class BaseRequestAPI {
     // throw UnimplementedError();
     return Future.value(false);
   }
-// parse(Map<dynamic, dynamic> data);
+
+  /// 发起请求
+  Future<Map<String, dynamic>> fetch() async {
+    final response = await RequestManager().request(this);
+    return response;
+  }
+
+  /// 返回布尔值的数据请求
+  ///
+  /// onResult 根据字典返回 true 的判断条件
+  /// hasLoading 是否展示 loading
+  /// return (请求是否成功, 提示语)
+  Future<({bool isSuccess, String message})> fetchBool({
+    required bool Function(Map<String, dynamic> response)? onSuccess,
+    bool hasLoading = true,
+  }) async {
+    BaseRequestAPI api = this;
+    if (hasLoading) {
+      EasyToast.showLoading("请求中");
+    }
+    final response = await RequestManager().request(api);
+    if (hasLoading) {
+      EasyToast.hideLoading();
+    }
+    if (response.isEmpty) {
+      return (isSuccess: false, message: "");
+    }
+
+    String message = response["message"] ?? "";
+    final result = (response["result"] as bool?) ?? false;
+    final resultNew = onSuccess?.call(response) ?? result;
+
+    bool isSuccess = response["code"] == "OK" && resultNew;
+    return (isSuccess: isSuccess , message: message);
+  }
+
+  /// 返回列表类型请求接口
+  ///
+  /// onResult 根据字典返回 true 的判断条件
+  /// hasLoading 是否展示 loading
+  /// return (请求是否成功, 提示语, 数组)
+  Future<({bool isSuccess, String message, List<T> list})> fetchList<T extends Map<String, dynamic>>({
+    List<T> Function(Map<String, dynamic> response)? onList,
+  }) async {
+    BaseRequestAPI api = this;
+    final response = await RequestManager().request(api);
+    if (response.isEmpty) {
+      return (isSuccess: false, message: RequestMsg.networkErrorMsg, list: <T>[]);
+    }
+    bool isSuccess = response["code"] == "OK";
+    String message = response["message"] ?? "";
+    final result = (response["result"] as List<T>?) ?? <T>[];
+    final items = onList?.call(response) ?? result;
+    return (isSuccess: isSuccess, message: message, list: items);
+  }
+
 }
 
 
