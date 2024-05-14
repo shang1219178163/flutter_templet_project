6ef8b641b3f482b5d928752920cea3d4b532a05d
Shang <shang1219178163@gmail.com>
2024-05-14 11:51:02 +0800

    feat：新增app 全局控制器

--- lib/pages/app_tab_bar_controller.dart
+++ lib/pages/app_tab_bar_controller.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/cupertino.dart';
-
-abstract class AppTabBarController {
-  /// tab bar item 单击回调
-  void onBarTap(int index);
-
-  /// tab bar item 双击回调
-  void onBarDoubleTap(int index);
-}
+import 'package:get/get.dart';
 
 abstract class AppTabBarState<T extends StatefulWidget> extends State<T> {
   /// tab bar item 单击回调
@@ -16,4 +9,17 @@ abstract class AppTabBarState<T extends StatefulWidget> extends State<T> {
   /// tab bar item 双击回调
   @protected
   void onBarDoubleTap(int index) {}
+}
+
+class AppController extends GetxController {
+  // late final messageController = Get.find<MessageController>();
+  // late final userController = Get.find<UserController>();
+
+  /// 全局 app 前后台切换
+  var appState = AppLifecycleState.resumed.obs;
+
+  // /// 返回 appTabPage
+  // void backTabPage() {
+  //   Get.until((route) => route.settings.name == APPRouter.appTabPage);
+  // }
 }
