04393b76c99c0b082a108b85de9f6f8c67076f09
Shang <shang1219178163@gmail.com>
2024-06-12 14:23:49 +0800

    新增

---
+++ lib/mixin/timings_mixin.dart
@@ -0,0 +1,32 @@
+//
+//  TimingsMixin.dart
+//  yl_patient_app
+//
+//  Created by shang on 2024/6/12 11:12.
+//  Copyright © 2024/6/12 shang. All rights reserved.
+//
+
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/scheduler.dart';
+
+/// fps
+mixin TimingsMixin<T extends StatefulWidget> on State<T> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addTimingsCallback(onTimings);
+  }
+
+  @override
+  void dispose() {
+    SchedulerBinding.instance.removeTimingsCallback(onTimings);
+    super.dispose();
+  }
+
+  void onTimings(List<FrameTiming> timings) {
+    throw UnimplementedError(
+        "❌: $this 未实现 onTimings(List<FrameTiming> timings)");
+  }
+}
