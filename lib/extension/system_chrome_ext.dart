7e4fda7ccb8f214bd75ae0d584c855b7c0cff1b6
Shang <shang1219178163@gmail.com>
2024-06-13 21:57:26 +0800

    feat：新增扩展文件

---
+++ lib/extension/system_chrome_ext.dart
@@ -0,0 +1,19 @@
+//
+//  SystemChromeExt.dart
+//  flutter_templet_project
+//
+//  Created by shang on 2024/6/13 21:55.
+//  Copyright © 2024/6/13 shang. All rights reserved.
+//
+
+import 'package:flutter/services.dart';
+
+extension SystemChromeExt on SystemChrome {
+  // 强制竖屏
+  static Future<void> setDeviceOrientationPortrait() {
+    return SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+}
