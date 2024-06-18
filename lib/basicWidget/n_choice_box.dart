d720bb4534e7673f16754f3974200c53a9dec6de
Shang <shang1219178163@gmail.com>
2024-06-18 16:14:41 +0800

    feat：主题设置调整

--- lib/basicWidget/n_choice_box.dart
+++ lib/basicWidget/n_choice_box.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_templet_project/extension/build_context_ext.dart';
@@ -7,8 +5,7 @@ import 'package:flutter_templet_project/util/color_util.dart';
 
 ///选择盒子
 class NChoiceBox<T> extends StatefulWidget {
-
-  NChoiceBox({
+  const NChoiceBox({
     super.key,
     required this.items,
     required this.onChanged,
@@ -26,7 +23,7 @@ class NChoiceBox<T> extends StatefulWidget {
 
   /// 是否单选
   final bool isSingle;
-  
+
   final List<ChoiceBoxModel<T>> items;
 
   final ValueChanged<List<ChoiceBoxModel<T>>> onChanged;
@@ -40,33 +37,37 @@ class NChoiceBox<T> extends StatefulWidget {
   final WrapAlignment wrapAlignment;
 
   final EdgeInsets itemMargin;
+
   /// item 圆角
   final double itemRadius;
+
   /// 元素背景色
   final Color itemColor;
+
   /// 选中元素背景色
   final Color itemSelectedColor;
+
   /// 子项样式自定义
   final Widget? Function(T e, bool isSelected)? itemBuilder;
-
 
   @override
   _NChoiceBoxState<T> createState() => _NChoiceBoxState<T>();
 }
 
 class _NChoiceBoxState<T> extends State<NChoiceBox<T>> {
-
   /// 选中的 items
-  List<ChoiceBoxModel<T>> get selectedItems => widget.items.where((e) => e.isSelected == true).toList();
+  List<ChoiceBoxModel<T>> get selectedItems =>
+      widget.items.where((e) => e.isSelected).toList();
 
-  List<String> get selectedItemNames => selectedItems.map((e) => e.title).toList();
+  List<String> get selectedItemNames =>
+      selectedItems.map((e) => e.title).toList();
 
   @override
   void initState() {
     super.initState();
   }
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -77,7 +78,11 @@ class _NChoiceBoxState<T> extends State<NChoiceBox<T>> {
           spacing: widget.wrapSpacing,
           runSpacing: widget.wrapRunSpacing,
           alignment: widget.wrapAlignment,
-          children: widget.items.map((e) => buildItem(e),).toList(),
+          children: widget.items
+              .map(
+                (e) => buildItem(e),
+              )
+              .toList(),
         ),
       ],
     );
@@ -103,30 +108,35 @@ class _NChoiceBoxState<T> extends State<NChoiceBox<T>> {
         child: widget.itemBuilder?.call(e.data!, e.isSelected),
       );
     }
-    
-    return Theme(
-      data: ThemeData(
-        canvasColor: Colors.transparent,
-        highlightColor: Colors.white,
-      ),
-      child: ChoiceChip(
+
+    return ChipTheme(
+      data: ChipTheme.of(context).copyWith(
+        backgroundColor: Colors.transparent,
+        selectedColor: Colors.transparent,
+        elevation: 0,
         pressElevation: 0,
         showCheckmark: false,
-        label: Text(e.title,
+        padding: EdgeInsets.zero,
+      ),
+      child: ChoiceChip(
+        label: Text(
+          e.title,
           style: TextStyle(
-            color: e.isSelected == true ? Colors.white : fontColor.withOpacity(0.8),
+            color: e.isSelected ? Colors.white : fontColor.withOpacity(0.8),
             fontSize: 14,
           ),
         ),
-        padding: EdgeInsets.zero,
         labelPadding: EdgeInsets.symmetric(horizontal: 6),
         materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        selected: e.isSelected == true,
+        selected: e.isSelected,
         selectedColor: widget.itemSelectedColor,
         // backgroundColor: widget.itemColor,
-        backgroundColor: e.isSelected ? widget.itemSelectedColor : Color(0xffF3F3F3),
+        backgroundColor:
+            e.isSelected ? widget.itemSelectedColor : Color(0xffF3F3F3),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(widget.itemRadius),),
+          borderRadius: BorderRadius.all(
+            Radius.circular(widget.itemRadius),
+          ),
           side: BorderSide(color: Colors.transparent),
         ),
         onSelected: (bool selected) {
@@ -150,7 +160,6 @@ class _NChoiceBoxState<T> extends State<NChoiceBox<T>> {
 
 /// ChoiceBox组件匹配模型
 class ChoiceBoxModel<T> {
-
   ChoiceBoxModel({
     required this.title,
     required this.id,
