45946ce7c531eccdf604b3f7e8a4593d0815200c
Shang <shang1219178163@gmail.com>
2024-06-21 21:43:04 +0800

    feat：新增类型

--- lib/extension/custom_type_util.dart
+++ lib/extension/custom_type_util.dart
@@ -21,3 +21,9 @@ typedef ValueChangedWidgetBuilder<T> = Widget Function(
 /// e 泛型数据;
 /// action 事件回调;
 typedef ActionRecord<T> = ({T e, VoidCallback action});
+
+/// 选择菜单子元素的元祖;
+/// title 标题
+/// value 泛型数据;
+/// title 所选元素对应接口参数;
+typedef ChooseItemRecord<T> = ({String title, String key, T value});
