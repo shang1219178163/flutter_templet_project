1bb7eec85ff5526764146bcf11b56a69f6c2e3cd
Shang <shang1219178163@gmail.com>
2024-06-28 10:43:01 +0800

    代码格式化

--- lib/pages/demo/IconConvertPage.dart
+++ lib/pages/demo/IconConvertPage.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -12,11 +10,7 @@ import 'package:tuple/tuple.dart';
 import 'package:path_provider/path_provider.dart';
 
 class IconConvertPage extends StatefulWidget {
-
-  IconConvertPage({
-    super.key,
-    this.title
-  });
+  IconConvertPage({super.key, this.title});
 
   final String? title;
 
@@ -25,7 +19,6 @@ class IconConvertPage extends StatefulWidget {
 }
 
 class _IconConvertPageState extends State<IconConvertPage> {
-
   final scrollController = ScrollController();
   final scrollController1 = ScrollController();
   final scrollController2 = ScrollController();
@@ -37,41 +30,52 @@ class _IconConvertPageState extends State<IconConvertPage> {
 
   final linesThreeVN = ValueNotifier(<String>[]);
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title ?? "$widget"),
-        actions: ['done',].map((e) => TextButton(
-          onPressed: onDone,
-          child: Text(e,
-            style: TextStyle(color: Colors.white),
-          ),
-        )
-        ).toList(),
+        actions: [
+          'done',
+        ]
+            .map((e) => TextButton(
+                  onPressed: onDone,
+                  child: Text(
+                    e,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ))
+            .toList(),
       ),
       body: NPageView(
         items: [
-          Tuple2("全部", buildPage(
-            items: linesVN,
-            controller: scrollController,
-            onConvert: convertToMapItem,
-          )),
-          Tuple2("IconData get", buildPage(
-            items: linesOneVN,
-            controller: scrollController1,
-            onConvert: convertToMapItem,
-          )),
-          Tuple2("const IconData ", buildPage(
-            items: linesTwoVN,
-            controller: scrollController2,
-            onConvert: convertToMapItem,
-          )),
-          Tuple2("结果", buildPage(
-            items: linesThreeVN,
-            controller: scrollController3,
-          )),
+          Tuple2(
+              "全部",
+              buildPage(
+                items: linesVN,
+                controller: scrollController,
+                onConvert: convertToMapItem,
+              )),
+          Tuple2(
+              "IconData get",
+              buildPage(
+                items: linesOneVN,
+                controller: scrollController1,
+                onConvert: convertToMapItem,
+              )),
+          Tuple2(
+              "const IconData ",
+              buildPage(
+                items: linesTwoVN,
+                controller: scrollController2,
+                onConvert: convertToMapItem,
+              )),
+          Tuple2(
+              "结果",
+              buildPage(
+                items: linesThreeVN,
+                controller: scrollController3,
+              )),
         ],
       ),
     );
@@ -87,8 +91,7 @@ class _IconConvertPageState extends State<IconConvertPage> {
         Expanded(
           child: ValueListenableBuilder(
               valueListenable: items,
-              builder: (context, list, child){
-
+              builder: (context, list, child) {
                 if (list.isEmpty) {
                   return Center(child: NPlaceholder());
                 }
@@ -99,44 +102,50 @@ class _IconConvertPageState extends State<IconConvertPage> {
                       controller: controller,
                       itemCount: list.length,
                       itemBuilder: (_, i) {
-
                         final e = list[i];
 
                         // final subtitle = convertToMapItem(e);
                         final subtitle = onConvert?.call(e) ?? "";
 
                         return Container(
-                          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 4),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                NText(e,
+                                NText(
+                                  e,
                                   textAlign: TextAlign.start,
                                 ),
-                                if(subtitle.isNotEmpty)NText(subtitle,
-                                  textAlign: TextAlign.start,
-                                  color: Colors.black45,
-                                ),
+                                if (subtitle.isNotEmpty)
+                                  NText(
+                                    subtitle,
+                                    textAlign: TextAlign.start,
+                                    color: Colors.black45,
+                                  ),
                               ],
-                            )
-                        );
-                      }
-                  ),
+                            ));
+                      }),
                 );
-              }
-          ),
+              }),
         ),
       ],
     );
   }
 
   onDone() async {
-    var lines = await readFile(path: '/Users/shang/fvm/versions/3.16.7/packages/flutter/lib/src/material/icons.dart');
-    lines = lines.where((e) => e.contains("IconData get") || e.contains("const IconData ")).toList();
+    var lines = await readFile(
+        path:
+            '/Users/shang/fvm/versions/3.16.7/packages/flutter/lib/src/material/icons.dart');
+    lines = lines
+        .where(
+            (e) => e.contains("IconData get") || e.contains("const IconData "))
+        .toList();
     linesVN.value = lines;
 
     linesOneVN.value = lines.where((e) => e.contains("IconData get")).toList();
-    linesTwoVN.value = lines.where((e) => e.contains("const IconData ")).toList();
+    linesTwoVN.value =
+        lines.where((e) => e.contains("const IconData ")).toList();
     debugPrint("$widget linesOneVN: ${linesOneVN.value.length},");
     debugPrint("$widget linesTwoVN: ${linesTwoVN.value.length},");
 
@@ -144,9 +153,7 @@ class _IconConvertPageState extends State<IconConvertPage> {
     debugPrint("$widget linesThreeVN: ${linesThreeVN.value.length},");
 
     final keyValues = linesThreeVN.value.map((e) => "\t$e\n").join("");
-    createFile(
-        fileName: "icons_map",
-        content: """
+    createFile(fileName: "icons_map", content: """
 import 'package:flutter/material.dart';
   
   
@@ -181,8 +188,7 @@ Map<String, IconData> kIConDic = {
 
       final desc = "${tmp[0]}.${tmp[1]}";
       result = "'$desc': $desc,";
-
-    } else if(e.contains("const IconData ")){
+    } else if (e.contains("const IconData ")) {
       tmp = tmp.sublist(4, 6);
 
       final desc = "${tmp[0]}.${tmp[1]}";
@@ -197,11 +203,14 @@ Map<String, IconData> kIConDic = {
   }
 
   createFile({String? fileName, required String content}) async {
-    final file = await FileManager().createFile(fileName: fileName, content: content);
+    fileName ??= "未命名_${DateTime.now().toString().substring(0, 9)}";
+    final file =
+        await FileManager().createFile(fileName: fileName, content: content);
     debugPrint("file: ${file.path}");
 
     showSnackBar(SnackBar(
-      content: NText("文件已生成(下载文件夹)",
+      content: NText(
+        "文件已生成(下载文件夹)",
         color: Colors.white,
         textAlign: TextAlign.center,
       ),
