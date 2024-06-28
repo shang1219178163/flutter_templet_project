466d80d925927adb886dc173086914836f58c3b0
Shang <shang1219178163@gmail.com>
2024-06-28 10:42:07 +0800

    feat:  新增布尔类型扩展

---
+++ lib/extension/bool_ext.dart
@@ -0,0 +1,23 @@
+//
+//  BoolExt.dart
+//  yl_ylgcp_app
+//
+//  Created by shang on 2024/6/25 09:38.
+//  Copyright © 2024/6/25 shang. All rights reserved.
+//
+
+extension BoolExt on bool {
+  /// 转字符串
+  String toValue({String trueValue = "Y", String falseValue = "N"}) {
+    return this ? trueValue : falseValue;
+  }
+}
+
+extension BoolStringExt on String {
+  /// 转字符串
+  bool toBool({List<String> trueValues = const ["y", "true"]}) {
+    final val = toLowerCase();
+    final result = trueValues.contains(val);
+    return result;
+  }
+}
