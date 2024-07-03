3bebf81137e1101f966ebf02855974b035737063
Shang <shang1219178163@gmail.com>
2024-07-03 11:33:01 +0800

    feat: 硬件功能扩展

--- lib/util/device_info_plugin_ext.dart
+++ lib/util/device_info_plugin_ext.dart
@@ -12,13 +12,19 @@ import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 
 extension DeviceInfoPluginExt on DeviceInfoPlugin {
+  /// 是否是模拟器
+  FutureOr<bool> isSimulator() async {
+    final result = await isPhysicalDevice();
+    return !result;
+  }
+
   /// 是真机(true)
   FutureOr<bool> isPhysicalDevice() async {
-    final deviceInfoPlugin = DeviceInfoPlugin();
     if (!Platform.isIOS && !Platform.isAndroid) {
-      return true;
+      return false;
     }
 
+    final deviceInfoPlugin = DeviceInfoPlugin();
     if (Platform.isIOS) {
       final deviceInfo = await deviceInfoPlugin.iosInfo;
       return deviceInfo.isPhysicalDevice;
@@ -26,5 +32,21 @@ extension DeviceInfoPluginExt on DeviceInfoPlugin {
 
     final deviceInfo = await deviceInfoPlugin.androidInfo;
     return deviceInfo.isPhysicalDevice;
+  }
+
+  /// 是pad
+  FutureOr<bool> isPad() async {
+    if (!Platform.isIOS && !Platform.isAndroid) {
+      return false;
+    }
+
+    final deviceInfoPlugin = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      final deviceInfo = await deviceInfoPlugin.iosInfo;
+      return deviceInfo.utsname.machine.contains("iPad");
+    }
+
+    final deviceInfo = await deviceInfoPlugin.androidInfo;
+    return deviceInfo.model.contains("Pad");
   }
 }
