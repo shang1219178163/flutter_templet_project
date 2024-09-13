c2bee81bdeb67ee7c7c7f087aeb2eb000236d186
Shang <shang1219178163@gmail.com>
2024-09-13 21:48:46 +0800

    fix：音频播放退到后台暂停音频播放

--- lib/vendor/audioplayers/audio_player_bar.dart
+++ lib/vendor/audioplayers/audio_player_bar.dart
@@ -7,6 +7,7 @@ import 'package:flutter_templet_project/extension/ddlog.dart';
 import 'package:flutter_templet_project/extension/duration_ext.dart';
 import 'package:flutter_templet_project/extension/string_ext.dart';
 import 'package:flutter_templet_project/extension/widget_ext.dart';
+import 'package:flutter_templet_project/mixin/safe_set_state_mixin.dart';
 import 'package:flutter_templet_project/util/color_util.dart';
 import 'package:path/path.dart';
 
@@ -32,7 +33,8 @@ class AudioPlayerBar extends StatefulWidget {
   State<StatefulWidget> createState() => _AudioPlayerBarState();
 }
 
-class _AudioPlayerBarState extends State<AudioPlayerBar> {
+class _AudioPlayerBarState extends State<AudioPlayerBar>
+    with WidgetsBindingObserver {
   AudioPlayer player = AudioPlayer();
   PlayerState? _playerState;
   late Duration? _duration = widget.duration;
@@ -51,6 +53,7 @@ class _AudioPlayerBarState extends State<AudioPlayerBar> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
 
     player.setReleaseMode(ReleaseMode.stop);
 
@@ -89,6 +92,7 @@ class _AudioPlayerBarState extends State<AudioPlayerBar> {
 
   @override
   void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
     player.stop();
     player.dispose();
     _durationSubscription?.cancel();
@@ -96,6 +100,37 @@ class _AudioPlayerBarState extends State<AudioPlayerBar> {
     _playerCompleteSubscription?.cancel();
     _playerStateChangeSubscription?.cancel();
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    // debugPrint("$widget didChangeAppLifecycleState: $state");
+
+    switch (state) {
+      //进入应用时候不会触发该状态 应用程序处于可见状态，并且可以响应用户的输入事件。它相当于 Android 中Activity的onResume
+      case AppLifecycleState.resumed:
+        // print("应用进入前台======");
+        _play();
+        break;
+      //应用状态处于闲置状态，并且没有用户的输入事件，
+      // 注意：这个状态切换到 前后台 会触发，所以流程应该是先冻结窗口，然后停止UI
+      case AppLifecycleState.inactive:
+        // print("应用处于闲置状态，这种状态的应用应该假设他们可能在任何时候暂停 切换到后台会触发======");
+        _pause();
+        break;
+      //当前页面即将退出
+      case AppLifecycleState.detached:
+        // print("当前页面即将退出======");
+        break;
+      // 应用程序处于不可见状态
+      case AppLifecycleState.paused:
+        // print("应用处于不可见状态 后台======");
+        break;
+      case AppLifecycleState.hidden:
+        // print("AppLifecycleState- hidden");
+        break;
+    }
   }
 
   @override
