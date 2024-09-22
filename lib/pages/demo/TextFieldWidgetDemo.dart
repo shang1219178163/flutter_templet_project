e77b8793462ece1eff4860394c99abf9da1daae6
Shang <shang1219178163@gmail.com>
2024-09-22 09:02:33 +0800

    fix：显示优化

--- lib/pages/demo/TextFieldWidgetDemo.dart
+++ lib/pages/demo/TextFieldWidgetDemo.dart
@@ -33,7 +33,7 @@ class TextFieldWidgetDemo extends StatefulWidget {
 
 class _TextFieldWidgetDemoState extends State<TextFieldWidgetDemo> {
   bool get hideApp =>
-      "$widget".endsWith(Get.currentRoute.replaceFirst("/", ""));
+      Get.currentRoute.toLowerCase() != "/$widget".toLowerCase();
 
   final _scrollController = ScrollController();
 
