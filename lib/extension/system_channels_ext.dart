36714e4826a565d97ef7809bd39f7a54c4a0e21d
Shang <shang1219178163@gmail.com>
2024-12-21 09:52:12 +0800

    feat： 新增 SystemChannels 扩展文件

---
+++ lib/extension/system_channels_ext.dart
@@ -0,0 +1,21 @@
+//
+//  SystemChannelsExt.dart
+//  flutter_templet_project
+//
+//  Created by shang on 2024/12/18 16:24.
+//  Copyright © 2024/12/18 shang. All rights reserved.
+//
+
+import 'package:flutter/services.dart';
+
+extension SystemChannelsExt on SystemChannels {
+  /// 键盘展示
+  Future<T?> textInputShow<T>() {
+    return SystemChannels.textInput.invokeMethod<T>('TextInput.show');
+  }
+
+  /// 键盘隐藏
+  Future<T?> textInputHide<T>() {
+    return SystemChannels.textInput.invokeMethod<T>('TextInput.hide');
+  }
+}
