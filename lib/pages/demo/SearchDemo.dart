c8c2ecc7c16f7a93ab08b9a2e1d0359e67e77510
Shang <shang1219178163@gmail.com>
2025-02-15 11:41:44 +0800

    refactor：尺寸优化

--- lib/pages/demo/SearchDemo.dart
+++ lib/pages/demo/SearchDemo.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/basicWidget/n_network_image.dart';
+import 'package:flutter_templet_project/enum/ActivityType.dart';
+import 'package:flutter_templet_project/extension/ddlog.dart';
+import 'package:flutter_templet_project/util/R.dart';
 
 class SearchDemo extends StatefulWidget {
   SearchDemo({Key? key, this.title}) : super(key: key);
@@ -39,43 +43,84 @@ class _SearchDemoState extends State<SearchDemo> {
   }
 
   Widget buildSearchAnchor() {
-    return SearchAnchor(builder: (context, controller) {
-      return SearchBar(
-        controller: controller,
-        padding: const MaterialStatePropertyAll<EdgeInsets>(
-            EdgeInsets.symmetric(horizontal: 16.0)),
-        onTap: () {
-          controller.openView();
-        },
-        onChanged: (_) {
-          // controller.openView();
-        },
-        leading: const Icon(Icons.search),
-        trailing: <Widget>[
-          Tooltip(
-            message: 'Change brightness mode',
-            child: IconButton(
-              isSelected: false,
-              onPressed: () {
-                setState(() {});
-              },
-              icon: const Icon(Icons.wb_sunny_outlined),
-              selectedIcon: const Icon(Icons.brightness_2_outlined),
-            ),
-          )
-        ],
-      );
-    }, suggestionsBuilder: (context, controller) {
-      return List<ListTile>.generate(5, (int index) {
-        final item = 'item $index';
-        return ListTile(
-          title: Text(item),
+    return SearchAnchor(
+      viewConstraints: BoxConstraints.loose(Size.fromHeight(200)),
+      builder: (context, controller) {
+        final isSelected = ValueNotifier(true);
+        return SearchBar(
+          controller: controller,
+          padding: const MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.symmetric(horizontal: 16.0)),
           onTap: () {
-            controller.closeView(item);
-            setState(() {});
+            controller.openView();
+          },
+          onChanged: (_) {
+            // controller.openView();
+          },
+          leading: const Icon(Icons.search),
+          trailing: <Widget>[
+            Tooltip(
+              message: 'Change brightness mode',
+              child: ValueListenableBuilder(
+                valueListenable: isSelected,
+                builder: (context, value, child) {
+                  DLog.d("isSelected: ${isSelected.value}");
+
+                  final icon = value ? const Icon(Icons.brightness_2_outlined) : const Icon(Icons.wb_sunny_outlined);
+                  return IconButton(
+                    onPressed: () {
+                      isSelected.value = !isSelected.value;
+                    },
+                    icon: icon,
+                  );
+                },
+              ),
+            )
+          ],
+        );
+      },
+      suggestionsBuilder: (context, controller) {
+        return List<Widget>.generate(
+          5,
+          (int i) {
+            final item = 'item $i';
+            return buildItem(
+                url: R.image.urls[i],
+                title: item,
+                onTap: () {
+                  controller.closeView(item);
+                  setState(() {});
+                });
           },
         );
-      });
-    });
+      },
+      viewBuilder: (suggestions) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+              child: Text("${suggestions.length}"),
+            ),
+            ...suggestions.toList(),
+            Expanded(child: SizedBox()),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildItem({required String url, required String title, VoidCallback? onTap}) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: ListTile(
+        leading: NNetworkImage(
+          url: url,
+          width: 45,
+        ),
+        title: Text(title),
+        onTap: onTap,
+      ),
+    );
   }
 }
