97326687df48a26efc964e6e584549d1e91cdb94
Shang <shang1219178163@gmail.com>
2025-03-19 09:26:45 +0800

    feat：测试 Get.put 的方法重复导入特性

--- lib/provider/getx_state_demo.dart
+++ lib/provider/getx_state_demo.dart
@@ -7,6 +7,7 @@
 //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/extension/ddlog.dart';
 import 'package:get/get.dart';
 
 class GetxStateDemo extends StatelessWidget {
@@ -14,41 +15,52 @@ class GetxStateDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// 通过依赖注入方式实例化的控制器
-    final counter = Get.put(CounterController());
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("GetX"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(() => Text(
-                  "count的值为：${counter.count}",
-                  style: const TextStyle(color: Colors.redAccent, fontSize: 20),
-                )),
-            GetX<CounterController>(
-              init: counter,
-              builder: (controller) {
-                return Text(
-                  "count的值为：${controller.count}",
-                  style: const TextStyle(color: Colors.redAccent, fontSize: 20),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              // 按钮点击count值++
-              onPressed: () {
-                counter.increase();
-              },
-              child: const Text("点击count++"),
-            ),
-          ],
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    /// 通过依赖注入方式实例化的控制器
+    final counter = Get.put(CounterController());
+    final counter1 = Get.put(CounterController());
+    final isOne = (counter == counter1); //重复put时不会重复插入,可通过此方式避免使用时没有注入的问题;
+    DLog.d([counter.hashCode, counter1.hashCode, isOne].asMap());
+
+    return Scrollbar(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(() => Text(
+                    "count的值为：${counter.count}",
+                    style: const TextStyle(color: Colors.redAccent, fontSize: 20),
+                  )),
+              GetX<CounterController>(
+                init: counter,
+                builder: (controller) {
+                  return Text(
+                    "count的值为：${controller.count}",
+                    style: const TextStyle(color: Colors.redAccent, fontSize: 20),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                // 按钮点击count值++
+                onPressed: () {
+                  counter.increase();
+                },
+                child: const Text("点击count++"),
+              ),
+            ],
+          ),
         ),
       ),
     );
