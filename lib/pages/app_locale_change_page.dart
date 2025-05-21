f0d29096a2cabb4c3f699a0c6a4f1f1d5ae9d485
Shang <shang1219178163@gmail.com>
2025-05-21 11:08:31 +0800

    feat：代码优化

--- lib/pages/app_locale_change_page.dart
+++ lib/pages/app_locale_change_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter_templet_project/extension/build_context_ext.dart';
 import 'package:flutter_templet_project/extension/dlog.dart';
 import 'package:flutter_templet_project/main.dart';
 import 'package:flutter_templet_project/util/color_util.dart';
-import 'package:get/get.dart';
+import 'package:get/get.dart' hide Trans;
 
 class AppLocaleChangePage extends StatefulWidget {
   const AppLocaleChangePage({
@@ -39,82 +39,63 @@ class _AppLocaleChangePageState extends State<AppLocaleChangePage> {
       appBar: hideApp
           ? null
           : AppBar(
-              title: Text("$widget"),
-              actions: [
-                'done',
-              ]
-                  .map((e) => TextButton(
-                        child: Text(
-                          e,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () => debugPrint(e),
-                      ))
-                  .toList(),
+              title: Text("多语言"),
             ),
       body: buildBody(),
     );
   }
 
   Widget buildBody() {
-    return ListView.separated(
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          final textStyle = TextStyle(
-            fontSize: 12,
-          );
-          return Container(
-            child: Column(
-              children: [
-                Text('money'.plural(10), style: textStyle), // 显示 "10 dollars"
-                Text('money'.plural(1), style: textStyle), // 显示 "one dollar"
-                Text('money'.plural(0), style: textStyle), // 显示 "no money"
-
-                Text(
-                    'money'.plural(
-                      10.23,
-                      namedArgs: {'name': 'Jane'},
-                    ),
-                    style: textStyle), // 显示 "no money"
-              ],
-            ),
-          );
-        }
-
-        final e = context.supportedLocales[index - 1];
-        final isSelected = e == context.locale;
-        return ListTile(
-          onTap: () async {
-            await context.setLocale(e);
-            Get.updateLocale(e);
-          },
-          title: Text(e.languageCode),
-          trailing: Icon(
-            Icons.check,
-            color: isSelected ? context.primaryColor : Colors.transparent,
-          ),
-        );
-      },
-      separatorBuilder: (context, index) {
-        return Divider(height: 1, color: lineColor);
-      },
-      itemCount: context.supportedLocales.length + 1,
-    );
-  }
-
-  Widget buildBody1() {
     return Scrollbar(
       controller: scrollController,
-      child: SingleChildScrollView(
-        controller: scrollController,
-        child: Column(
-          children: [
-            ...context.supportedLocales.map((e) => ListTile(
-                  title: Text(e.languageCode),
-                )),
-            // Text(context.locale.languageCode),
-          ],
-        ),
+      child: ListView.separated(
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            final textStyle = TextStyle(
+              fontSize: 12,
+            );
+            return Container(
+              child: Column(
+                children: [
+                  Text('language'.tr(), style: textStyle),
+                  Text(
+                      'money'.plural(
+                        0,
+                        namedArgs: {
+                          'name': 'Jane',
+                        },
+                      ),
+                      style: textStyle),
+                  Text(
+                      plural(
+                        'money',
+                        10.23,
+                        namedArgs: {'name': 'Jane', 'money': '10.23'},
+                      ),
+                      style: textStyle),
+                ],
+              ),
+            );
+          }
+
+          final e = context.supportedLocales[index - 1];
+          final isSelected = e == context.locale;
+          return ListTile(
+            onTap: () async {
+              await context.setLocale(e);
+              Get.updateLocale(e);
+            },
+            title: Text(e.languageCode),
+            trailing: Icon(
+              Icons.check,
+              color: isSelected ? context.primaryColor : Colors.transparent,
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider(height: 1, color: lineColor);
+        },
+        itemCount: context.supportedLocales.length + 1,
       ),
     );
   }
