98455e7701063dab22cccf7be46869b8c52b47ab
Shang <shang1219178163@gmail.com>
2025-09-11 15:00:56 +0800

    feat: 新增工具类

---
+++ lib/extension/asset_bundle_ext.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+extension AssetBundleExt on AssetBundle {
+  static Future<bool> checkAssetExists(String assetPath) async {
+    try {
+      // 尝试加载资源文件
+      await rootBundle.load(assetPath);
+      return true;
+    } catch (e) {
+      // 如果加载失败，则文件不存在
+      return false;
+    }
+  }
+}
