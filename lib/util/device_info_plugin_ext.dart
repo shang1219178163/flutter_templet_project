0fd7bcd896c40c5405ccac2481dd4d21a14fb791
Shang <shang1219178163@gmail.com>
2025-12-29 11:45:33 +0800

    feat：代码重构

--- lib/util/device_info_plugin_ext.dart
+++ lib/util/device_info_plugin_ext.dart
@@ -12,60 +12,60 @@ import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 
 extension DeviceInfoPluginExt on DeviceInfoPlugin {
-  /// 是否是模拟器
+  /// 支持的平台
+  static bool get supportPlatforms => [Platform.isIOS, Platform.isAndroid].contains(true);
+
+  /// 获取机器信息
+  static FutureOr<T> getDeviceInfo<T>({
+    required T Function(IosDeviceInfo iosInfo) iosCb,
+    required T Function(AndroidDeviceInfo androidInfo) androidCb,
+  }) async {
+    final deviceInfoPlugin = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      final deviceInfo = await deviceInfoPlugin.iosInfo;
+      return iosCb(deviceInfo);
+    }
+    final deviceInfo = await deviceInfoPlugin.androidInfo;
+    return androidCb(deviceInfo);
+  }
+
+  /// 是真机
+  static FutureOr<bool> isPhysicalDevice() async {
+    if (!supportPlatforms) {
+      return false;
+    }
+    return getDeviceInfo(
+      iosCb: (v) => v.isPhysicalDevice,
+      androidCb: (v) => v.isPhysicalDevice,
+    );
+  }
+
+  /// 是模拟器
   static FutureOr<bool> isSimulator() async {
     final result = await isPhysicalDevice();
     return !result;
   }
 
-  /// 是真机
-  static FutureOr<bool> isPhysicalDevice() async {
-    final supports = [Platform.isIOS, Platform.isAndroid];
-    if (!supports.contains(true)) {
-      return false;
-    }
-
-    final deviceInfoPlugin = DeviceInfoPlugin();
-    if (Platform.isIOS) {
-      final deviceInfo = await deviceInfoPlugin.iosInfo;
-      return deviceInfo.isPhysicalDevice;
-    }
-
-    final deviceInfo = await deviceInfoPlugin.androidInfo;
-    return deviceInfo.isPhysicalDevice;
-  }
-
   /// 获取设备Id
   static FutureOr<String> getDeviceId() async {
-    final supports = [Platform.isIOS, Platform.isAndroid];
-    if (!supports.contains(true)) {
+    if (!supportPlatforms) {
       return "";
     }
-
-    final deviceInfoPlugin = DeviceInfoPlugin();
-    if (Platform.isIOS) {
-      final deviceInfo = await deviceInfoPlugin.iosInfo;
-      return deviceInfo.identifierForVendor ?? "";
-    }
-
-    final deviceInfo = await deviceInfoPlugin.androidInfo;
-    return deviceInfo.display;
+    return getDeviceInfo(
+      iosCb: (v) => v.identifierForVendor ?? "",
+      androidCb: (v) => v.display,
+    );
   }
 
   /// 是pad
   static FutureOr<bool> isPad() async {
-    final supports = [Platform.isIOS, Platform.isAndroid];
-    if (!supports.contains(true)) {
+    if (!supportPlatforms) {
       return false;
     }
 
-    final deviceInfoPlugin = DeviceInfoPlugin();
-    if (Platform.isIOS) {
-      final deviceInfo = await deviceInfoPlugin.iosInfo;
-      return deviceInfo.utsname.machine.contains("iPad");
-    }
-
-    final deviceInfo = await deviceInfoPlugin.androidInfo;
-    return deviceInfo.model.contains("Pad");
+    return getDeviceInfo(
+      iosCb: (v) => v.utsname.machine.contains("iPad"),
+      androidCb: (v) => v.model.contains("Pad"),
+    );
   }
 }
