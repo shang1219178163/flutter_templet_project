f86497fea306e06c51142eabf34e7d5de0bb6551
Shang <shang1219178163@gmail.com>
2026-01-30 18:14:54 +0800

    feat：CustomScrollView 添加 EasyRefresh

--- lib/pages/sliver_demo/SliverPersistentHeaderDemoTwo.dart
+++ lib/pages/sliver_demo/SliverPersistentHeaderDemoTwo.dart
@@ -1,11 +1,13 @@
 import 'package:easy_refresh/easy_refresh.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_templet_project/basicWidget/n_network_image.dart';
 import 'package:flutter_templet_project/basicWidget/n_pair.dart';
 import 'package:flutter_templet_project/basicWidget/n_resize_switch.dart';
 import 'package:flutter_templet_project/basicWidget/n_sliver_persistent_header_delegate.dart';
 import 'package:flutter_templet_project/basicWidget/n_text.dart';
 import 'package:flutter_templet_project/extension/extension_local.dart';
 import 'package:flutter_templet_project/util/AppRes.dart';
+import 'package:flutter_templet_project/util/theme/app_color.dart';
 import 'package:get/get.dart';
 
 class SliverPersistentHeaderDemoTwo extends StatefulWidget {
@@ -21,17 +23,15 @@ class SliverPersistentHeaderDemoTwo extends StatefulWidget {
 }
 
 class _SliverPersistentHeaderDemoTwoState extends State<SliverPersistentHeaderDemoTwo> {
-  bool get hideApp => "$widget".toLowerCase().endsWith(Get.currentRoute.toLowerCase());
-
   final _scrollController = ScrollController();
 
-  Map<String, dynamic> arguments = Get.arguments ?? <String, dynamic>{};
+  late final refreshController = EasyRefreshController(
+    controlFinishRefresh: true,
+    controlFinishLoad: true,
+  );
 
-  /// id
-  late final id = arguments["id"];
-
-  bool pinned = true;
-  bool floating = true;
+  bool pinned = false;
+  bool floating = false;
 
   @override
   void didUpdateWidget(covariant SliverPersistentHeaderDemoTwo oldWidget) {
@@ -41,29 +41,56 @@ class _SliverPersistentHeaderDemoTwoState extends State<SliverPersistentHeaderDe
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.bgColorF9F9F9,
       appBar: AppBar(
         title: Text(widget.toString()),
       ),
-      body: EasyRefresh(
-        onRefresh: () {
-          DLog.d("onRefresh");
+      body: NestedScrollView(
+        headerSliverBuilder: (context, bool innerBoxIsScrolled) {
+          return [
+            buildPersistentHeader(),
+          ];
         },
-        onLoad: () {
-          DLog.d("onLoad");
-        },
-        child: CustomScrollView(
-          slivers: [
-            buildHeader(),
-            buildListView(),
-          ],
+        body: EasyRefresh.builder(
+          controller: refreshController,
+          onRefresh: onRefresh,
+          onLoad: onLoad,
+          childBuilder: (_, physics) {
+            return CustomScrollView(
+              physics: physics,
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Container(
+                    height: 40,
+                    color: Colors.green,
+                  ),
+                ),
+                buildListView(),
+              ],
+            );
+          },
         ),
       ),
     );
   }
 
-  Widget buildHeader() {
+  Future<void> onRefresh() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    refreshController.finishRefresh();
+    refreshController.resetFooter();
+    DLog.d("onRefresh");
+  }
+
+  Future<void> onLoad() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    refreshController.finishLoad();
+    DLog.d("onLoad");
+  }
+
+  Widget buildPersistentHeader() {
     var min = context.paddingTop;
     var max = 250.0;
+
     return NSliverPersistentHeaderBuilder(
       pinned: pinned,
       floating: floating,
@@ -82,93 +109,101 @@ class _SliverPersistentHeaderDemoTwoState extends State<SliverPersistentHeaderDe
 
         var opacity = (1.0 - progress).clamp(0.0, 1.0);
 
-        return Stack(
-          fit: StackFit.expand,
-          children: [
-            // 背景图片
-            Image.network(
-              AppRes.image.urls[5],
-              fit: BoxFit.cover,
-            ),
-            // 带有渐变的遮罩层
-            Container(
-              color: Colors.black.withOpacity(0.3),
-            ),
-            if (progress >= 0.5)
-              AnimatedOpacity(
-                opacity: 1.0 - opacity,
-                duration: Duration(milliseconds: 100),
-                child: Text(
-                  "Resizable Header ${{
-                    "shrinkOffset": shrinkOffset.toStringAsFixed(2),
-                    "leaveOffset": leaveOffset.toStringAsFixed(2),
-                    "progress": progress.toStringAsFixed(2),
-                  }} ",
-                  style: TextStyle(
-                    color: Colors.white,
-                    // fontSize: titleSize,
+        return DefaultTextStyle(
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              // 背景图片
+              NNetworkImage(
+                url: AppRes.image.urls[5],
+                fit: BoxFit.cover,
+              ),
+              // 带有渐变的遮罩层
+              Container(
+                color: Colors.black.withOpacity(0.3),
+              ),
+              if (progress >= 0.5)
+                AnimatedOpacity(
+                  opacity: 1.0 - opacity,
+                  duration: Duration(milliseconds: 100),
+                  child: Text(
+                    "Resizable Header ${{
+                      "shrinkOffset": shrinkOffset.toStringAsFixed(2),
+                      "leaveOffset": leaveOffset.toStringAsFixed(2),
+                      "progress": progress.toStringAsFixed(2),
+                    }} ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      // fontSize: titleSize,
+                    ),
                   ),
                 ),
-              ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(color: Colors.yellow, width: 1),
-              ),
-              child: AnimatedOpacity(
-                opacity: opacity,
-                duration: Duration(milliseconds: 100),
-                child: OverflowBox(
-                  maxWidth: context.width,
-                  maxHeight: max,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Resizable Header ${{
-                          "shrinkOffset": shrinkOffset.toStringAsFixed(2),
-                          "leaveOffset": leaveOffset.toStringAsFixed(2),
-                          "progress": progress.toStringAsFixed(2),
-                        }} ",
-                        style: TextStyle(
-                          color: Colors.white,
-                          // fontSize: titleSize,
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.yellow, width: 1),
+                ),
+                child: AnimatedOpacity(
+                  opacity: opacity,
+                  duration: Duration(milliseconds: 100),
+                  child: OverflowBox(
+                    maxWidth: context.width,
+                    maxHeight: max,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Resizable Header ${{
+                            "shrinkOffset": shrinkOffset.toStringAsFixed(2),
+                            "leaveOffset": leaveOffset.toStringAsFixed(2),
+                            "progress": progress.toStringAsFixed(2),
+                          }} ",
+                          style: TextStyle(
+                            color: Colors.white,
+                            // fontSize: titleSize,
+                          ),
                         ),
-                      ),
-                      buildSwitch(
-                        name: "pinned",
-                        value: pinned,
-                        onChanged: (val) {
-                          pinned = val;
-                          setState(() {});
-                        },
-                      ),
-                      buildSwitch(
-                        name: "floating",
-                        value: floating,
-                        onChanged: (val) {
-                          floating = val;
-                          setState(() {});
-                        },
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: List.generate(
+                        buildSwitch(
+                          name: "pinned",
+                          value: pinned,
+                          onChanged: (val) {
+                            pinned = val;
+                            setState(() {});
+                          },
+                        ),
+                        buildSwitch(
+                          name: "floating",
+                          value: floating,
+                          onChanged: (val) {
+                            floating = val;
+                            setState(() {});
+                          },
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: List.generate(
                               5,
-                              (index) => Container(
-                                    child: Text(
-                                      "项目$index",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  )),
+                              (index) {
+                                return Container(
+                                  child: Text(
+                                    "项目$index",
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
                         ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
@@ -187,14 +222,19 @@ class _SliverPersistentHeaderDemoTwoState extends State<SliverPersistentHeaderDe
 
   Widget buildSwitch({
     required String name,
+    Color? textColor,
     required bool value,
     ValueChanged<bool>? onChanged,
   }) {
     return NPair(
       icon: NText(
         name,
+        style: TextStyle(
+          color: textColor,
+        ),
       ),
       child: NResizeSwitch(
+        height: 30,
         value: value,
         onChanged: onChanged,
       ),
