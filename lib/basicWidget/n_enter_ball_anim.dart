9825218aa30dc1a83875f73e09829f495a47a1fb
Shang <shang1219178163@gmail.com>
2026-02-14 10:22:16 +0800

    refactor：组件增强

--- lib/basicWidget/n_enter_ball_anim.dart
+++ lib/basicWidget/n_enter_ball_anim.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_templet_project/util/theme/app_color.dart';
 
+/// 进球闪烁动画
 class NEnterBallAnim extends StatelessWidget {
   const NEnterBallAnim({
     super.key,
     required this.valueListenable,
     this.textAlign,
-    this.style,
+    this.backgroundColorBuilder,
+    this.styleBuilder,
     required this.child,
   });
 
   final ValueNotifier<bool> valueListenable;
   final TextAlign? textAlign;
-  final TextStyle Function(bool isEnter)? style;
+  final Color Function(bool isEnter)? backgroundColorBuilder;
+  final TextStyle Function(bool isEnter)? styleBuilder;
 
   final Widget child;
 
@@ -26,23 +29,27 @@ class NEnterBallAnim extends StatelessWidget {
     return ValueListenableBuilder(
       valueListenable: valueListenable,
       builder: (context, value, _) {
+        final bgColorDefault = value ? AppColor.cancelColor : Colors.transparent;
+        final bgColor = backgroundColorBuilder?.call(value) ?? bgColorDefault;
+        final style = styleBuilder?.call(value) ??
+            TextStyle(
+              color: value ? Colors.white : fontColor,
+              fontSize: 16,
+              fontFamily: 'DingTalk',
+            );
+
         return Container(
           padding: const EdgeInsets.symmetric(horizontal: 2),
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: value ? AppColor.cancelColor : Colors.transparent,
+            color: bgColor,
             borderRadius: BorderRadius.circular(3),
           ),
           child: DefaultTextStyle(
             textAlign: textAlign,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
-            style: style?.call(value) ??
-                TextStyle(
-                  color: value ? Colors.white : fontColor,
-                  fontSize: 16,
-                  fontFamily: 'DingTalk',
-                ),
+            style: style,
             child: child,
           ),
         );
