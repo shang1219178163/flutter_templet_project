aebaf422f2b4b6ec3ab8044b7807b24adbb71bdb
Flutter_b <shang1219178163@gmail.com>
2026-05-12 11:16:42 +0800

    feat：移除 NInputAccessoryViewNew

--- lib/basicWidget/text_field/n_input_accessory_view.dart
+++ lib/basicWidget/text_field/n_input_accessory_view.dart
@@ -6,17 +6,16 @@
 //  Copyright © 2026/4/29 shang. All rights reserved.
 //
 
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 import 'package:flutter_templet_project/basicWidget/elevated_btn.dart';
 import 'package:flutter_templet_project/basicWidget/overlay/n_overlay_manager_new.dart';
-import 'package:flutter_templet_project/util/dlog.dart';
+import 'package:flutter_templet_project/util/n_screen_manager.dart';
 import 'package:flutter_templet_project/util/theme/app_color.dart';
 
 /// 键盘辅助视图
-class NInputAccessoryView extends StatelessWidget {
+class NInputAccessoryView extends StatefulWidget {
   const NInputAccessoryView({
     super.key,
     this.focusNode,
@@ -55,7 +54,6 @@ class NInputAccessoryView extends StatelessWidget {
     required BuildContext context,
     FocusNode? focusNode,
     required TextEditingController controller,
-    ValueNotifier<double>? keyboardVN,
     TextInputType? keyboardType,
     String? hintText = "请输入",
     List<TextInputFormatter>? inputFormatters,
@@ -64,66 +62,24 @@ class NInputAccessoryView extends StatelessWidget {
     double? keyboardHeight,
   }) {
     focusNode?.requestFocus();
-    SystemChannels.textInput.invokeMethod('TextInput.show');
-
-    if (keyboardVN == null) {
-      NOverlayManagerNew.show(
-        context,
-        autoDismiss: false,
-        builder: (c) {
-          var bottom = keyboardHeight ?? MediaQuery.of(c).viewInsets.bottom;
-          DLog.d("bottom: $bottom");
-
-          return Positioned(
-            left: 0,
-            right: 0,
-            bottom: bottom,
-            child: NInputAccessoryView(
-              focusNode: focusNode,
-              controller: controller,
-              keyboardType: keyboardType,
-              hintText: hintText,
-              maxLines: maxLines,
-              maxLength: maxLength,
-              inputFormatters: inputFormatters,
-              onConfirm: (v) {
-                NInputAccessoryView.dismiss();
-              },
-            ),
-          );
-        },
-      );
-      return;
-    }
+    // SystemChannels.textInput.invokeMethod('TextInput.show');
     NOverlayManagerNew.show(
       context,
       autoDismiss: false,
       builder: (c) {
-        var bottom = keyboardHeight ?? MediaQuery.of(c).viewInsets.bottom;
-        DLog.d("bottom: $bottom");
-
-        return ValueListenableBuilder(
-          valueListenable: keyboardVN,
-          child: NInputAccessoryView(
-            focusNode: focusNode,
-            controller: controller,
-            keyboardType: keyboardType,
-            hintText: hintText,
-            maxLines: maxLines,
-            maxLength: maxLength,
-            inputFormatters: inputFormatters,
-            onConfirm: (v) {
-              NInputAccessoryView.dismiss();
-            },
-          ),
-          builder: (context, value, child) {
-            return AnimatedPositioned(
-              duration: Duration(milliseconds: 100),
-              left: 0,
-              right: 0,
-              bottom: max(keyboardVN.value, bottom),
-              child: child!,
-            );
+        // var bottom = keyboardHeight ?? MediaQuery.of(c).viewInsets.bottom;
+        // DLog.d("bottom: $bottom");
+        // DLog.d("NInputAccessoryViewNew");
+        return NInputAccessoryView(
+          focusNode: focusNode,
+          controller: controller,
+          keyboardType: keyboardType,
+          hintText: hintText,
+          maxLines: maxLines,
+          maxLength: maxLength,
+          inputFormatters: inputFormatters,
+          onConfirm: (v) {
+            dismiss();
           },
         );
       },
@@ -135,77 +91,92 @@ class NInputAccessoryView extends StatelessWidget {
   }
 
   @override
+  State<NInputAccessoryView> createState() => _NInputAccessoryViewState();
+}
+
+class _NInputAccessoryViewState extends State<NInputAccessoryView>
+    with KeyboardHeightChangedMixin<NInputAccessoryView> {
+  @override
   Widget build(BuildContext context) {
     final textField = TextField(
       onTapOutside: (e) {
-        if (onTapOutside != null) {
-          onTapOutside?.call(e);
+        if (widget.onTapOutside != null) {
+          widget.onTapOutside?.call(e);
           return;
         }
         NInputAccessoryView.dismiss();
       },
-      focusNode: focusNode,
-      controller: controller,
-      inputFormatters: inputFormatters,
+      focusNode: widget.focusNode,
+      controller: widget.controller,
+      inputFormatters: widget.inputFormatters,
       minLines: 1,
-      maxLines: maxLines,
-      maxLength: maxLength,
-      keyboardType: keyboardType,
+      maxLines: widget.maxLines,
+      maxLength: widget.maxLength,
+      keyboardType: widget.keyboardType,
       style: const TextStyle(fontSize: 16),
       decoration: InputDecoration(
         border: InputBorder.none,
         // isDense: true,
         counterText: '',
         contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6).copyWith(bottom: 8),
-        hintText: hintText,
+        hintText: widget.hintText,
       ),
     );
 
-    final textFieldNew = textFieldBuilder?.call(textField) ?? textField;
+    final textFieldNew = widget.textFieldBuilder?.call(textField) ?? textField;
 
-    return Container(
-      // height: 50,
-      // margin: EdgeInsets.only(bottom: bottom),
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 7),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        // border: Border.all(color: Colors.blue),
-        border: Border(top: BorderSide(color: AppColor.lineColor, width: 0.5)),
-      ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 4, right: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFFF6F6F6),
-                  borderRadius: BorderRadius.circular(8),
+    return ValueListenableBuilder(
+      valueListenable: keyboardHeightVN,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 7),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          // border: Border.all(color: Colors.blue),
+          border: Border(top: BorderSide(color: AppColor.lineColor, width: 0.5)),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 4, right: 8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF6F6F6),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: textFieldNew,
                 ),
-                child: textFieldNew,
               ),
             ),
-          ),
-          ValueListenableBuilder(
-            valueListenable: controller,
-            builder: (context, value, child) {
-              final disable = value.text.isEmpty;
-              return ElevatedBtn(
-                height: 40,
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                radius: 20,
-                onPressed: disable
-                    ? null
-                    : () {
-                        onConfirm(value.text);
-                        focusNode?.unfocus();
-                      },
-                title: "确定",
-              );
-            },
-          ),
-        ],
+            ValueListenableBuilder(
+              valueListenable: widget.controller,
+              builder: (context, value, child) {
+                final disable = value.text.isEmpty;
+                return ElevatedBtn(
+                  height: 40,
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  radius: 20,
+                  onPressed: disable
+                      ? null
+                      : () {
+                          widget.onConfirm(value.text);
+                          widget.focusNode?.unfocus();
+                        },
+                  title: "确定",
+                );
+              },
+            ),
+          ],
+        ),
       ),
+      builder: (context, bottom, child) {
+        return Positioned(
+          left: 0,
+          right: 0,
+          bottom: bottom,
+          child: child!,
+        );
+      },
     );
   }
 }
