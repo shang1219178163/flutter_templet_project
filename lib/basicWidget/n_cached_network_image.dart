c23195d50a49adfd16435de24b31d6a335ec41d6
Flutter_b <shang1219178163@gmail.com>
2026-05-30 17:23:06 +0800

    feat：新增CachedNetworkImage 缓存图片

---
+++ lib/basicWidget/n_cached_network_image.dart
@@ -0,0 +1,47 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/widgets.dart';
+
+/// 网络图片
+class NCachedNetworkImage extends StatelessWidget {
+  const NCachedNetworkImage({
+    super.key,
+    required this.imageUrl,
+    this.placeholder,
+    required this.width,
+    required this.height,
+    this.fit = BoxFit.cover,
+    this.color,
+    this.colorBlendMode,
+  });
+
+  final String imageUrl;
+  final AssetImage? placeholder;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
+  final Color? color;
+  final BlendMode? colorBlendMode;
+
+  @override
+  Widget build(BuildContext context) {
+    final placeholderWidget = Image(
+      image: placeholder ?? const AssetImage("assets/images/img_placeholder.png"),
+      fit: fit,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+    );
+
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      fit: fit,
+      width: width,
+      height: height,
+      color: color,
+      colorBlendMode: colorBlendMode,
+      placeholder: (_, url) => placeholderWidget,
+      errorWidget: (_, url, e) => placeholderWidget,
+    );
+  }
+}
