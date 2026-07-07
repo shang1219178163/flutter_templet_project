8edde1dc47882df27e657e448e7c08fa214ea9f3
Flutter_b <shang1219178163@gmail.com>
2026-07-07 18:42:06 +0800

    feat：补充圆角

--- lib/basicWidget/n_cached_network_image.dart
+++ lib/basicWidget/n_cached_network_image.dart
@@ -9,6 +9,7 @@ class NCachedNetworkImage extends StatelessWidget {
     this.placeholder = const AssetImage("assets/images/img_placeholder.png"),
     required this.width,
     required this.height,
+    this.radius = 0,
     this.fit = BoxFit.cover,
     this.color,
     this.colorBlendMode,
@@ -18,19 +19,35 @@ class NCachedNetworkImage extends StatelessWidget {
   final AssetImage placeholder;
   final double? width;
   final double? height;
+  final double? radius;
   final BoxFit? fit;
   final Color? color;
   final BlendMode? colorBlendMode;
 
   @override
   Widget build(BuildContext context) {
-    final placeholderWidget = Image(
-      image: placeholder,
-      fit: fit,
+    // final placeholderWidget = Image(
+    //   image: placeholder,
+    //   fit: fit,
+    //   width: width,
+    //   height: height,
+    //   color: color,
+    //   colorBlendMode: colorBlendMode,
+    // );
+
+    final blendModeNew = colorBlendMode ?? BlendMode.srcIn;
+    final colorFilter = color == null ? null : ColorFilter.mode(color!, blendModeNew);
+    final placeholderWidget = Container(
       width: width,
       height: height,
-      color: color,
-      colorBlendMode: colorBlendMode,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius ?? 0),
+        image: DecorationImage(
+          image: placeholder,
+          fit: fit,
+          colorFilter: colorFilter,
+        ),
+      ),
     );
 
     return CachedNetworkImage(
@@ -38,10 +55,23 @@ class NCachedNetworkImage extends StatelessWidget {
       fit: fit,
       width: width,
       height: height,
+      memCacheWidth: (width?.toInt() ?? 0) * 3,
+      memCacheHeight: (height?.toInt() ?? 0) * 3,
       color: color,
-      colorBlendMode: colorBlendMode,
+      colorBlendMode: blendModeNew,
       placeholder: (_, url) => placeholderWidget,
       errorWidget: (_, url, e) => placeholderWidget,
+      imageBuilder: (context, imageProvider) {
+        return DecoratedBox(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(radius ?? 0),
+            image: DecorationImage(
+              image: imageProvider,
+              fit: fit,
+            ),
+          ),
+        );
+      },
     );
   }
 }
